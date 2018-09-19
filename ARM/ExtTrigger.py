a078bd107a8275af57cd848f27f4e1cec68760cb
Mark Zieg <mark@zieg.com>
2018-09-19 15:26:48 -0400

    simplified ExtTrigger.py (still works)

--- ARM/ExtTrigger.py
+++ ARM/ExtTrigger.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python -u
 
 ################################################################################
-# This worked:
-#
-# $ ./ExtTrigger.py --trigger-source HW --continuous-acquisition --frames-per-trigger 3 --reset 
+# Worked with no arguments on S-00247 ARM:
 # uC Revision:       10.0.0.3
 # FPGA Revision:     008-007
 ################################################################################
@@ -11,6 +9,7 @@
 import sys
 import usb.core
 import argparse
+import datetime
 from time import sleep
 
 ################################################################################
@@ -22,7 +21,6 @@ PID            = 0x4000
 HOST_TO_DEVICE = 0x40
 DEVICE_TO_HOST = 0xC0
 ZZ             = [0] * 8
-TIMEOUT_MS     = 10000
 PIXELS         = 1024
 
 ################################################################################
@@ -30,7 +28,7 @@ PIXELS         = 1024
 ################################################################################
 
 def Get_Value(Command, ByteCount, raw=False):
-    RetArray = dev.ctrl_transfer(DEVICE_TO_HOST, Command, 0, 0, ByteCount, TIMEOUT_MS)
+    RetArray = dev.ctrl_transfer(DEVICE_TO_HOST, Command, 0, 0, ByteCount, args.timeout_ms)
     if raw:
         RetVal = RetArray
     else:
@@ -43,7 +41,7 @@ def Test_Set(SetCommand, GetCommand, SetValue, RetLen):
     lsb   =  SetValue        & 0xffff
     msb   = (SetValue >> 16) & 0xffff
     ZZ[0] = (SetValue >> 32) & 0xff
-    result = dev.ctrl_transfer(HOST_TO_DEVICE, SetCommand, lsb, msb, ZZ, TIMEOUT_MS)
+    result = dev.ctrl_transfer(HOST_TO_DEVICE, SetCommand, lsb, msb, ZZ, args.timeout_ms)
     if result != len(ZZ):
         return 'Set %02x failed' % SetCommand
     else:
@@ -59,20 +57,9 @@ def getFirmwareRev():
 def getFPGARev():
     return "".join(chr(x) for x in Get_Value(0xb4, 7, raw=True))
 
-def reset():
-    print "Integration Time: ", Test_Set(0xb2, 0xbf, args.integration_time_ms, 6) 
-
-    # configure trigger source
-    if PID != 0x4000: # not needed on ARM, per https://github.com/WasatchPhotonics/WasatchUSB/issues/2
-
-        print "Continuous Acq:   ", Test_Set(0xc8, 0xcc, (1 if args.continuous_acquisition else 0), 1) 
-        print "Continuous Frames:", Test_Set(0xc9, 0xcd, args.frames_per_trigger, 1) 
-
-        if args.trigger_source == "SW":
-            print "Trigger Source:   ", Test_Set(0xd2, 0xd3, 0, 1)
-        elif args.trigger_source == "HW":
-            print "Trigger Source:   ", Test_Set(0xd2, 0xd3, 1, 1) 
-
+def display(msg):
+    now = datetime.datetime.now()
+    print "%s: %s" % (now, msg)
 
 ################################################################################
 # main()
@@ -80,13 +67,11 @@ def reset():
 
 # support SW triggering just so we can confirm the script is otherwise working
 parser = argparse.ArgumentParser()
-parser.add_argument("--integration-time-ms",    type=int, default=100,    help="integration time (ms)")
-parser.add_argument("--trigger-source",         type=str, default='NONE', help="specify trigger source", choices=['NONE', 'SW', 'HW'])
-parser.add_argument("--frames-per-trigger",     type=int, default=1,      help="how many frames to collect per trigger")
-parser.add_argument("--max-errors",             type=int, default=3,      help="number of permitted USB read errors")
-parser.add_argument("--continuous-acquisition", action='store_true',      help="whether to collect multiple spectra per trigger")
-parser.add_argument("--reset",                  action='store_true',      help="forcibly reset continuous acquisition after each trigger")
+parser.add_argument ("--integration-time-ms", type=int, default=100,    help="integration time (ms)")
+parser.add_argument ("--trigger-source",      type=str, default='NONE', help="specify trigger source", choices=['NONE', 'SW', 'HW'])
+parser.add_argument ("--timeout-sec",         type=int, default=20,     help="USB timeout (sec)")
 args = parser.parse_args()
+args.timeout_ms = args.timeout_sec * 1000
 
 # claim USB device
 dev = usb.core.find(idVendor=VID, idProduct=PID)
@@ -97,40 +82,21 @@ if not dev:
 # initialize test
 print "uC Revision:      ", getFirmwareRev()
 print "FPGA Revision:    ", getFPGARev()
-reset()
+print "Integration Time: ", Test_Set(0xb2, 0xbf, args.integration_time_ms, 6) 
+print "Timeout MS:       ", args.timeout_ms
 
-print "\nstart sending incoming trigger signals..."
+# loop over trigger signals
+print "\nstart sending incoming trigger signals...\n"
 sleep(5)
 
-# loop over acquisitions
-frame_count = 0
-error_count = 0
+frames = 0
 while True:
-    print "expecting next trigger"
-
     if args.trigger_source == "SW":
-        print "sending SW trigger"
-        dev.ctrl_transfer(HOST_TO_DEVICE, 0xad, 0, 0, ZZ, TIMEOUT_MS)
+        display("sending SW trigger")
+        dev.ctrl_transfer(HOST_TO_DEVICE, 0xad, 0, 0, ZZ, args.timeout_ms)
         
-    for i in range(args.frames_per_trigger):
-        print "Waiting for data...(20 second timeout)"
+    display("waiting for data")
+    data = dev.read(0x82, 2 * PIXELS, args.timeout_ms) 
 
-        # this try-catch block doesn't help anything
-        try:
-            data = dev.read(0x82, 2 * PIXELS, 20000) # 20sec timeout
-        except Exception as exc:
-            if error_count < args.max_errors:
-                print "  (ignoring USB error)"
-                error_count += 1
-            else:
-                raise exc
-
-        print("Read frame %d" % frame_count)
-        frame_count += 1
-
-    # this reset doesn't seem to help anything
-    if args.reset:
-        print "Continuous Acq:   ", Test_Set(0xc8, 0xcc, 0, 1) # disable
-        sleep(1)
-        reset() # re-enable, if configured
-        sleep(1)
+    display("read frame %d" % frames)
+    frames += 1
