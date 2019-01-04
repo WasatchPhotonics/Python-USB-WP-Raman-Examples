7c7ebf53c0d005c21719b834fe5a37eee196c43c
Mark Zieg <mark@zieg.com>
2019-01-04 15:42:20 -0500

    added 3rd byte to battery state

--- SiG/get_battery_state.py
+++ SiG/get_battery_state.py
@@ -20,17 +20,20 @@ TIMEOUT_MS = 1000
 def Get_Value(Command, command2, ByteCount, index=0):
     return dev.ctrl_transfer(DEVICE_TO_HOST, Command, command2, 0, ByteCount, TIMEOUT_MS)
 
-# 2nd-tier opcode returns two bytes: [LSB, MSB]
+# 2nd-tier opcode returns three bytes: [LSB, MSB, state]
 #
 # MSB = integral battery charge level (0-100)
 # LSB = fractional battery charge level
 #         0 =   0 / 256 = 0.0000%
 #       255 = 255 / 256 = 0.9961%
+# state = 1 (charging) or 0 (not charging)
 #
-# E.g., [0x20, 0x40] = 64 + 32/256 = 64.125%
+# E.g., [0x20, 0x40, 0x00] = 64 + 32/256 = 64.125% (not charging)
 def get_battery_level():
-    battery_register = Get_Value(0xff, 0x13, 2)
-    battery_charge_level = battery_register[1] + (1.0 * battery_register[0] / 256.0)
-    return battery_charge_level
+    register = Get_Value(0xff, 0x13, 3)
+    percentage = register[1] + (1.0 * register[0] / 256.0)
+    charging = register[2] != 0
+    return (percentage, charging)
     
-print "Battery Charge Level: %.2f" % get_battery_level()
+(percentage, charging) = get_battery_level()
+print "Battery Charge Level: %.2f%% (%s)" % (percentage, "charging" if charging else "not charging")
