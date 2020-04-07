cfd8035d191ce86e0399c780df2eb1425d348652
Mark Zieg <mark@zieg.com>
2020-04-07 16:40:09 -0400

    check for duplicates

--- ARM/GetSpectra.py
+++ ARM/GetSpectra.py
@@ -9,26 +9,30 @@ from time import sleep
 # select product
 dev=usb.core.find(idVendor=0x24aa, idProduct=0x4000)
 
-print dev
-H2D=0x40
-D2H=0xC0
-BUFFER_SIZE=8
-Z=[0] * BUFFER_SIZE
-TIMEOUT=1000
+HOST_TO_DEVICE = 0x40
+DEVICE_TO_HOST = 0xC0
+BUFFER_SIZE = 8
+Z = [0] * BUFFER_SIZE
+TIMEOUT_MS = 1000
 
-# select pixel count
-PixelCount=1024
-PixelCount=1952
+pixels = 1952
+integration_time_ms = 10
 
-print "Start Data Acquisition"
-dev.ctrl_transfer(H2D, 0xad, 0,0,Z,TIMEOUT)   # trigger an acquisition
+print("Set integraiton time 10ms")
+dev.ctrl_transfer(HOST_TO_DEVICE, 0xb2, integration_time_ms & 0xffff, 0, Z, TIMEOUT_MS)
+
+print("Start Data Acquisition")
+dev.ctrl_transfer(HOST_TO_DEVICE, 0xad, 0, 0, Z, TIMEOUT_MS)
 
 # MZ: this works from Windows but not Mac?
-Data = dev.read(0x82, PixelCount*2)
+data = dev.read(0x82, pixels * 2)
 
-print "Read %d pixels (%d bytes)" % (PixelCount, len(Data))
-for j in range (0, (PixelCount*2)/32, 1):
-    for i in range (0, 31, 2):
-        pixel = Data[j*32+i+1]*256+Data[j*32+i]
-        sys.stdout.write("%04x " % pixel)
+print("Read %d pixels (%d bytes)" % (pixels, len(data)))
+last = -1
+for i in range(0, len(data), 2):
+    intensity = data[i] | (data[i+1] << 8)
+    sys.stdout.write("pixel %4d: 0x%04x (%d)" % (i / 2, intensity, intensity, ))
+    if last == intensity:
+        sys.stdout.write(" (duplicate)")
     sys.stdout.write("\n")
+    last = intensity
