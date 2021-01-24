45ab3d72185daf7d5814bfc319ac032a8023e48d
gajsewnath <gsewnath@wasatchphotonics.com>
2021-01-24 09:49:33 -0500

    Mark modified code to capture carry-overs during data reads.

--- FX2-Silicon/AreaScan.py
+++ FX2-Silicon/AreaScan.py
@@ -20,21 +20,54 @@ TIMEOUT=1000
 PixelCount=1024				
 #PixelCount=2048
 
+integ_time_ms = 5000
 
+print ("PixelCount: ", PixelCount)
 print ("Set integration time")
-dev.ctrl_transfer(H2D, 0xb2, 100, 0, Z, TIMEOUT)   # set integration time to 100ms
+dev.ctrl_transfer(H2D, 0xb2, integ_time_ms, 0, Z, TIMEOUT)   # set integration time
 
-print ("Start Area Scan")
+#print ("Start Area Scan")
 dev.ctrl_transfer(H2D, 0xeb, 1,0,Z,TIMEOUT)   # trigger an acquisition
 
+# print ("Start Data Acquisition")
+
+dev.ctrl_transfer(H2D, 0xad, 0,0,Z,TIMEOUT)   # trigger an acquisition
+
+bytesRead = 0
+bytesToRead = 2048
+spectrum = []
+fullSpectrum = []
+
 for row in range(70):
-    # print ("Start Data Acquisition")
-    dev.ctrl_transfer(H2D, 0xad, 0,0,Z,TIMEOUT)   # trigger an acquisition
-    Data = dev.read(0x82,PixelCount*2)
-    spectrum = []
-    for pixel in range(PixelCount):
-        lsb = Data[pixel*2]
-        msb = Data[pixel*2 + 1]
-        intensity = (msb << 8) | lsb
-        spectrum.append(intensity)
+    print("ROW: " , row)
+
+    #dev.ctrl_transfer(H2D, 0xad, 0,0,Z,TIMEOUT)   # trigger an acquisition
+    #sleep(0.00001)
+
+    while bytesRead < bytesToRead:
+    
+        nextSpectrum = []
+        Data = dev.read(0x82,PixelCount*2, TIMEOUT + integ_time_ms * 2)
+        print("Data (%d bytes): " % len(Data), Data)
+        #print("Data (%d bytes): " % len(Data))
+
+        for pixel in range(int(len(Data)/2)):
+            lsb = Data[pixel*2]
+            msb = Data[pixel*2 + 1]
+            intensity = (msb << 8) | lsb
+            
+            fullSpectrum.append(intensity)
+
+            if len(spectrum) < PixelCount:
+                spectrum.append(intensity)
+            else:
+                nextSpectrum.append(intensity)
+                
+            
+        bytesRead += len(Data)
+        
     print("Row %03d: %s .. %s" % (spectrum[0], spectrum[0:5], spectrum[PixelCount-6:PixelCount-1]))
+    spectrum = nextSpectrum
+    bytesRead -= bytesToRead
+    print("Carrying over from previous read: %d bytes (%d pixels)" % (bytesRead, len(spectrum)))
+#print("Full spectrum: %s " % fullSpectrum)
