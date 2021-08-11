79a6ef349e4441e7c9c2f679b06207013d1e6bf5
Mark Zieg <mark@zieg.com>
2021-08-10 21:13:33 -0400

    tightened, added delay

--- FX2-Silicon/AreaScan.py
+++ FX2-Silicon/AreaScan.py
@@ -1,73 +1,48 @@
 import usb.core
 import datetime
-import time
+import sys
+
 from time import sleep
 
-# select product
 dev=usb.core.find(idVendor=0x24aa, idProduct=0x1000)
-#dev=usb.core.find(idVendor=0x24aa, idProduct=0x2000)
-#dev=usb.core.find(idVendor=0x24aa, idProduct=0x4000)
+if dev is None:
+    print("No spectrometer found")
+    sys.exit(-1)
 
-print (dev)
-H2D=0x40
-D2H=0xC0
-BUFFER_SIZE=8
-Z='\x00'*BUFFER_SIZE
-TIMEOUT=1000
+H2D = 0x40
+D2H = 0xC0
+BUFFER_SIZE = 8
+Z = [0] * BUFFER_SIZE
+TIMEOUT = 1000
+DELAY_MS = 0 # value of 1 causes to eventually fall over
 
 # select pixel count
-#PixelCount=512
-PixelCount=1024				
-#PixelCount=2048
+pixels=1024				
+integ_time_ms = 1000
 
-integ_time_ms = 5000
+print("setting integration time %d ms" % integ_time_ms)
+dev.ctrl_transfer(H2D, 0xb2, integ_time_ms, 0, Z, TIMEOUT)   
 
-print ("PixelCount: ", PixelCount)
-print ("Set integration time")
-dev.ctrl_transfer(H2D, 0xb2, integ_time_ms, 0, Z, TIMEOUT)   # set integration time
+print("AREA_SCAN_ENABLE = 1")
+dev.ctrl_transfer(H2D, 0xeb, 1, 0, Z, TIMEOUT)   
 
-#print ("Start Area Scan")
-dev.ctrl_transfer(H2D, 0xeb, 1,0,Z,TIMEOUT)   # trigger an acquisition
+for loop in range(3):
 
-# print ("Start Data Acquisition")
+    print("ACQUIRE %d" % loop)
+    dev.ctrl_transfer(H2D, 0xad, 0, 0, Z, TIMEOUT)   
 
-dev.ctrl_transfer(H2D, 0xad, 0,0,Z,TIMEOUT)   # trigger an acquisition
+    for row in range(70):
+        spectrum = []
+        data = dev.read(0x82, pixels * 2, TIMEOUT + integ_time_ms * 2)
+        sleep(DELAY_MS * 0.001)
 
-bytesRead = 0
-bytesToRead = 2048
-spectrum = []
-fullSpectrum = []
-
-for row in range(70):
-    print("ROW: " , row)
-
-    #dev.ctrl_transfer(H2D, 0xad, 0,0,Z,TIMEOUT)   # trigger an acquisition
-    #sleep(0.00001)
-
-    while bytesRead < bytesToRead:
-    
-        nextSpectrum = []
-        Data = dev.read(0x82,PixelCount*2, TIMEOUT + integ_time_ms * 2)
-        print("Data (%d bytes): " % len(Data), Data)
-        #print("Data (%d bytes): " % len(Data))
-
-        for pixel in range(int(len(Data)/2)):
-            lsb = Data[pixel*2]
-            msb = Data[pixel*2 + 1]
+        for i in range(pixels):
+            lsb = data[i*2]
+            msb = data[i*2 + 1]
             intensity = (msb << 8) | lsb
-            
-            fullSpectrum.append(intensity)
+            spectrum.append(intensity)
 
-            if len(spectrum) < PixelCount:
-                spectrum.append(intensity)
-            else:
-                nextSpectrum.append(intensity)
-                
-            
-        bytesRead += len(Data)
-        
-    print("Row %03d: %s .. %s" % (spectrum[0], spectrum[0:5], spectrum[PixelCount-6:PixelCount-1]))
-    spectrum = nextSpectrum
-    bytesRead -= bytesToRead
-    print("Carrying over from previous read: %d bytes (%d pixels)" % (bytesRead, len(spectrum)))
-#print("Full spectrum: %s " % fullSpectrum)
+        print("Row %03d: %s .. %s" % (spectrum[0], spectrum[0:5], spectrum[pixels-6:pixels-1]))
+
+print("AREA_SCAN_ENABLE = 0")
+dev.ctrl_transfer(H2D, 0xeb, 0, 0, Z, TIMEOUT)   
