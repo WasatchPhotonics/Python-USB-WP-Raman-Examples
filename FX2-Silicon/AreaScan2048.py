d9de3e6b59b009401259fd3854672983ed50a418
Mark Zieg <mark@zieg.com>
2021-08-27 11:36:57 -0400

    debugging Area Scan on 2048px detectors

--- FX2-Silicon/AreaScan2048.py
+++ FX2-Silicon/AreaScan2048.py
@@ -12,13 +12,13 @@ if dev is None:
 H2D         = 0x40
 D2H         = 0xC0
 TIMEOUT     = 1000
-DELAY_MS    = 5     # value of 1 causes to eventually fall over
+DELAY_MS    = 4     # value of 1 causes to eventually fall over
 OFFSET_MS   = 5     # if that unit doesn't freak you out, you're not paying attention
 Z = [0] * 8
 
 # select pixel count
 pixels = 2048
-integ_time_ms = 1000
+integ_time_ms = 100
 
 print("setting integration time %d ms" % integ_time_ms)
 dev.ctrl_transfer(H2D, 0xb2, integ_time_ms, 0, Z, TIMEOUT)   
@@ -35,17 +35,27 @@ for loop in range(3):
     dev.ctrl_transfer(H2D, 0xad, 0, 0, Z, TIMEOUT)   
     for row in range(70):
         spectrum = []
+        subspectra = {}
         for ep in (0x82, 0x86):
+            subspectrum = []
             data = dev.read(ep, pixels, TIMEOUT + integ_time_ms * 2)
             for i in range(len(data) // 2):
                 lsb = data[i*2]
                 msb = data[i*2 + 1]
                 intensity = (msb << 8) | lsb
-                spectrum.append(intensity)
+                subspectrum.append(intensity)
+            subspectra[ep] = subspectrum
+            spectrum.extend(subspectrum)
             sleep(DELAY_MS * 1e-3)
 
-        len_ = len(spectrum)
-        print("Row %03d: %d pixels %s .. %s" % (spectrum[0], len_, spectrum[0:5], spectrum[len_-6:len_-1]))
+        row = spectrum[0]
+        print("Row %3d: %4d + %4d = %4d pixels %s .. %s" % (
+            row,
+            len(subspectra[0x82]), 
+            len(subspectra[0x86]),
+            len(spectrum), 
+            subspectra[0x82][0:5], 
+            subspectra[0x86][-5:]))
     print("frame received in %.2f sec" % (datetime.datetime.now() - time_start).total_seconds())
 print("AREA_SCAN_ENABLE = 0")
 dev.ctrl_transfer(H2D, 0xeb, 0, 0, Z, TIMEOUT)   
