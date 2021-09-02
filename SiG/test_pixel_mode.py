0041b8d936981b8d130b15ebca6c361cf2c6916e
Mark Zieg <mark@zieg.com>
2021-09-02 10:26:52 -0400

    added test_pixel_mode.py

---
+++ SiG/test_pixel_mode.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import usb.core
+import platform
+import argparse
+import matplotlib.pyplot as plt
+
+VID             = 0x24aa
+PID             = 0x4000
+BUF             = [0] * 8
+HOST_TO_DEVICE  = 0x40
+DEVICE_TO_HOST  = 0xC0
+TIMEOUT_MS      = 1000
+INTEGRATION_MS  = 400
+GAIN_DB         = 8
+PIXELS          = 1952
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--integration-time-ms", type=int, default=400)
+parser.add_argument("--gain-db",             type=int, default=8)
+parser.add_argument("--pixel-mode",          type=int, default=0, choices=[0,1,2,3])
+parser.add_argument("--plot",                action="store_true")
+args = parser.parse_args()
+
+dev = usb.core.find(idVendor=VID, idProduct=PID)
+if dev is None:
+    print(f"No matching spectrometer found (VID 0x{VID:04x}, PID 0x{PID:04x})")
+    sys.exit(1)
+
+if os.name == "posix":
+    dev.set_configuration()
+    usb.util.claim_interface(dev, 0)
+
+print(dev)
+
+print("sending SET_INTEGRATION_TIME_MS -> %d ms" % args.integration_time_ms)
+dev.ctrl_transfer(HOST_TO_DEVICE, 0xb2, args.integration_time_ms, 0, BUF, TIMEOUT_MS)
+
+gain_ff = args.gain_db << 8
+print(f"sending GAIN_DB -> {args.gain_db} (FunkyFloat 0x{gain_ff:04x})")
+dev.ctrl_transfer(HOST_TO_DEVICE, 0xb7, gain_ff, 0, BUF, TIMEOUT_MS) 
+
+print(f"setting pixel mode {args.pixel_mode}")
+dev.ctrl_transfer(HOST_TO_DEVICE, 0xfd, args.pixel_mode, 0, BUF, TIMEOUT_MS)
+
+print("sending ACQUIRE")
+dev.ctrl_transfer(HOST_TO_DEVICE, 0xad, 0, 0, BUF, TIMEOUT_MS)
+
+print("reading spectrum from bulk endpoint")
+data = dev.read(0x82, PIXELS * 2) 
+
+print("read %d bytes" % len(data))
+if len(data) != PIXELS * 2:
+    print("ERROR: read %d bytes" % len(data))
+    sys.exit(1)
+
+spectrum = []
+for i in range(PIXELS):
+    spectrum.append(data[i*2] | (data[i*2 + 1] << 8))
+
+if args.plot:
+    plt.plot(spectrum)
+    plt.title(f"integration time {args.integration_time_ms}ms, gain {args.gain_db}dB, pixel mode {args.pixel_mode}")
+    plt.show()
+else:
+    print(f"{len(spectrum)} pixels: {spectrum}")
