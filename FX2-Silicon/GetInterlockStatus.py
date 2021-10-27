57eafcc6ae57d0b7e99c7381da0eb5ad8ad2c41f
engineerleigh <lbrady@wasatchphotonics.com>
2021-10-27 14:28:56 -0400

    Added get interlock status.

---
+++ FX2-Silicon/GetInterlockStatus.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python -u
+
+import usb.core
+import datetime
+from time import sleep
+
+# Product selection
+dev=usb.core.find(idVendor=0x24aa, idProduct=0x1000)
+#dev=usb.core.find(idVendor=0x24aa, idProduct=0x2000)
+#dev=usb.core.find(idVendor=0x24aa, idProduct=0x4000)
+
+print (dev)
+H2D=0x40
+D2H=0xC0
+BUFFER_SIZE=8
+Z='\x00'*BUFFER_SIZE
+TIMEOUT=1000
+
+def Get_Value(Command, ByteCount, index=0):
+	RetVal = 0
+	RetArray = dev.ctrl_transfer(D2H, Command, 0,0,ByteCount,TIMEOUT)
+	for i in range (0, ByteCount):
+		RetVal = RetVal*256 + RetArray[ByteCount - i - 1]
+	return RetVal
+
+
+print ("Interlock:		",	Get_Value(0xef, 1),"		", 	dev.ctrl_transfer(D2H, 0xef, 0,0,1,TIMEOUT))
