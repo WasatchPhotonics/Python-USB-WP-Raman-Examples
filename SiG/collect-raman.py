3946ebae1df9e61f6bfd0ab28fcd8561e55c97ce
Mark Zieg <mark@zieg.com>
2022-02-10 10:30:55 -0500

    collect-raman seems to work well

--- SiG/collect-raman.py
+++ SiG/collect-raman.py
@@ -25,7 +25,6 @@ TIMEOUT_MS = 1000
 
 MAX_PAGES = 8
 PAGE_SIZE = 64
-EEPROM_FORMAT = 8
 
 # An extensible, stateful "Test Fixture" 
 class Fixture(object):
@@ -36,64 +35,72 @@ class Fixture(object):
 
     def __init__(self):
         self.outfile = None
+        self.device = None
         self.dark = None
 
+        # parse cmd-line args
         parser = argparse.ArgumentParser()
+        parser.add_argument("--bin2x2",              action="store_true", help="apply 2x2 binning")
         parser.add_argument("--count",               type=int,            help="read the given number of spectra (default 1)", default=1)
-        parser.add_argument("--dark",                action="store_true", help="collect dark and perform dark correction (default off)")
+        parser.add_argument("--dark",                action="store_true", help="collect dark and perform dark correction")
         parser.add_argument("--debug",               action="store_true", help="debug output (default off)")
         parser.add_argument("--delay-ms",            type=int,            help="delay n ms between spectra (default 10)", default=10)
+        parser.add_argument("--fire-laser",          action="store_true", help="to avoid accidents, WILL NOT fire laser unless specified")
         parser.add_argument("--gain-db",             type=float,          help="gain in dB (default 8.0)", default=8.0)
         parser.add_argument("--integration-time-ms", type=int,            help="integration time (ms) (default 100)", default=100)
         parser.add_argument("--laser-power",         type=int,            help="laser power as a percentage (range 1-100) (default 100)", default=100)
         parser.add_argument("--laser-warmup-ms",     type=int,            help="laser warmup delay in ms (default 4000)", default=4000)
         parser.add_argument("--outfile",             type=str,            help="outfile to save full spectra")
-        parser.add_argument("--plot",                action="store_true", help="graph spectra after collection (default off)")
+        parser.add_argument("--plot",                action="store_true", help="graph spectra after collection")
         parser.add_argument("--scans-to-average",    type=int,            help="scans to average (default 0)", default=1)
         self.args = parser.parse_args()
 
-        self.devices = usb.core.find(find_all=True, idVendor=0x24aa, idProduct=0x4000)
-        if len(self.devices) == 0:
+        # grab first spectrometer on the chain
+        device = usb.core.find(idVendor=0x24aa, idProduct=0x4000)
+        if device is None:
             print("No spectrometers found")
-        self.device = self.devices[0]
+            return
+        self.debug(device)
+        self.device = device
 
-        # claim device
-        self.debug("claiming spectrometer")
-        self.device.set_configuration(1)
-        usb.util.claim_interface(self.device, 0)
-        self.debug("claimed device")
+        # claim device (I'm never sure when this is required)
+        if False:
+            self.debug("claiming spectrometer")
+            self.device.set_configuration(1)
+            usb.util.claim_interface(self.device, 0)
+            self.debug("claimed device")
 
         # read configuration
         self.fw_version = self.get_firmware_version()
         self.fpga_version = self.get_fpga_version()
         self.read_eeprom()
         self.generate_wavelengths()
-        print(f"Connected to {self.eeprom['model']} {self.eeprom['serial_number']} with {self.eeprom['pixels']} ({self.wavelengths[0]}, {self.wavelengths[-1]}nm) ({self.wavenumbers[0]}, {self.wavenumbers[-1]}cm-1)")
+        print(f"Connected to {self.model} {self.serial_number} with {self.pixels} pixels ({self.wavelengths[0]:.2f}, {self.wavelengths[-1]:.2f}nm) ({self.wavenumbers[0]:.2f}, {self.wavenumbers[-1]:.2f}cm-1)")
+        print(f"ARM {self.fw_version}, FPGA {self.fpga_version}")
 
     def read_eeprom(self):
-        self.buffers = [self.get_cmd(self.device, 0xff, 0x01, page) for page in range(8)]
+        self.buffers = [self.get_cmd(0xff, 0x01, page) for page in range(8)]
 
         # parse key fields (extend as needed)
-        self.eeprom = {}
-        self.eeprom["format"]        = self.unpack((0, 63,  1), "B")
-        self.eeprom["model"]         = self.unpack((0,  0, 16), "s")
-        self.eeprom["serial_number"] = self.unpack((0, 16, 16), "s")
-        self.eeprom["pixels"]        = self.unpack((2, 16,  2), "H")
-        self.eeprom["excitation_nm"] = self.unpack((3, 36,  4), "f")
-        self.eeprom["wavecal_C0"]    = self.unpack((1,  0,  4), "f")
-        self.eeprom["wavecal_C1"]    = self.unpack((1,  4,  4), "f")
-        self.eeprom["wavecal_C2"]    = self.unpack((1,  8,  4), "f")
-        self.eeprom["wavecal_C3"]    = self.unpack((1, 12,  4), "f")
+        self.format         = self.unpack((0, 63,  1), "B")
+        self.model          = self.unpack((0,  0, 16), "s")
+        self.serial_number  = self.unpack((0, 16, 16), "s")
+        self.pixels         = self.unpack((2, 16,  2), "H")
+        self.excitation_nm  = self.unpack((3, 36,  4), "f")
+        self.wavecal_C0     = self.unpack((1,  0,  4), "f")
+        self.wavecal_C1     = self.unpack((1,  4,  4), "f")
+        self.wavecal_C2     = self.unpack((1,  8,  4), "f")
+        self.wavecal_C3     = self.unpack((1, 12,  4), "f")
 
     def generate_wavelengths(self):
         self.wavelengths = []
         self.wavenumbers = []
-        for i in range(self.eeprom["pixels"]):
-            wavelength = self.eeprom["wavecal_C0"] \
-                       + self.eeprom["wavecal_C1"] * i \
-                       + self.eeprom["wavecal_C2"] * i * i \
-                       + self.eeprom["wavecal_C3"] * i * i * i
-            wavenumber = 1e7 / self.eeprom["excitation_nm"] - 1e7 / wavelength
+        for i in range(self.pixels):
+            wavelength = self.wavecal_C0 \
+                       + self.wavecal_C1 * i \
+                       + self.wavecal_C2 * i * i \
+                       + self.wavecal_C3 * i * i * i
+            wavenumber = 1e7 / self.excitation_nm - 1e7 / wavelength
             self.wavelengths.append(wavelength)
             self.wavenumbers.append(wavenumber)
 
@@ -112,8 +119,12 @@ class Fixture(object):
         # set gain dB
         self.set_gain_db(self.args.gain_db)
 
+        # perform one throwaway (seems to help SiG)
+        self.get_spectrum()
+
         # take dark
         if self.args.dark:
+            print("taking dark")
             self.dark = self.get_averaged_spectrum()
 
         # open outfile
@@ -121,18 +132,21 @@ class Fixture(object):
             self.outfile = open(self.args.outfile, 'w')
 
             # header rows
-            self.outfile.write("pixel, %s\n" % (", ".join([x for x in range(self.eeprom["pixels"]))))
-            self.outfile.write("wavelength, %s\n" % (", ".join(["{x:f2}" for x in self.wavelengths])))
-            self.outfile.write("wavenumber, %s\n" % (", ".join(["{x:f2}" for x in self.wavenumbers])))
+            self.outfile.write("pixel, %s\n"      % (", ".join([str(x) for x in range(self.pixels)])))
+            self.outfile.write("wavelength, %s\n" % (", ".join([f"{x:.2f}" for x in self.wavelengths])))
+            self.outfile.write("wavenumber, %s\n" % (", ".join([f"{x:.2f}" for x in self.wavenumbers])))
 
         # enable laser
-        self.set_laser_enable(True)
+        if self.args.fire_laser:
+            self.set_laser_enable(True)
+        else:
+            print("*** not firing laser because --fire-laser not specified ***")
 
         # take measurements
         spectra = []
         try:
             for i in range(self.args.count):
-                # take measurement
+                # take dark-corrected measurement
                 spectrum = self.get_averaged_spectrum()
                 if self.dark is not None:
                     spectrum -= dark
@@ -140,14 +154,15 @@ class Fixture(object):
                 
                 # save measurement
                 now = datetime.now()
-                print("%s Spectrum %3d/%3d %s ..." % (now, i, self.args.count, spectrum[:10]))
-                if outfile is not None:
-                    self.outfile.write("%s, %s\n" % (now, ", ".join(["{x:f2}" for x in spectrum])))
+                print("%s Spectrum %3d/%3d %s ..." % (now, i+1, self.args.count, spectrum[:10]))
+                if self.outfile is not None:
+                    self.outfile.write("%s, %s\n" % (now, ", ".join([f"{x:.2f}" for x in spectrum])))
 
                 # delay before next
                 sleep(self.args.delay_ms / 1000.0 )
         except:
             print("caught exception reading spectra")
+            traceback.print_exc()
 
         # disable laser
         self.set_laser_enable(False)
@@ -160,7 +175,7 @@ class Fixture(object):
         if self.args.plot:
             for a in spectra:
                 plt.plot(a)
-            plt.title(f"integration time {args.integration_time_ms}ms, gain {args.gain_db}dB, count {args.count}")
+            plt.title(f"integration time {self.args.integration_time_ms}ms, gain {self.args.gain_db}dB, count {self.args.count}")
             plt.show()
 
     ############################################################################
@@ -183,13 +198,13 @@ class Fixture(object):
         return s
 
     def set_laser_enable(self, flag):
-        self.debug(f"setting laserEnable {flag}")
+        print(f"setting laserEnable {flag}")
         self.send_cmd(0xbe, 1 if flag else 0)
 
         if flag and self.args.laser_warmup_ms > 0:
-            self.debug("starting aser warmup")
+            print(f"{datetime.now()} starting laser warmup")
             sleep(self.args.laser_warmup_ms / 1000.0)
-            self.debug("finished aser warmup")
+            print(f"{datetime.now()} finished laser warmup")
 
     def set_integration_time_ms(self, ms):
         if ms < 1 or ms > 0xffff:
@@ -200,8 +215,11 @@ class Fixture(object):
         self.send_cmd(0xb2, ms)
 
     def set_gain_db(self, db):
-        raw = db << 8 
-        self.debug("setting gainDB 0x%04x (FunkyFloat)" % gainDB)
+        db = round(db, 1)
+        msb = int(db)
+        lsb = int((db - int(db)) * 10)
+        raw = (msb << 8) | lsb
+        self.debug("setting gainDB 0x%04x (FunkyFloat)" % raw)
         self.send_cmd(0xb7, raw)
 
     def set_modulation_enable(self, flag):
@@ -246,8 +264,8 @@ class Fixture(object):
 
     def get_spectrum(self):
         timeout_ms = TIMEOUT_MS + self.args.integration_time_ms * 2
-        self.send_cmd(self.device, 0xad, 1)
-        data = self.device.read(0x82, dev.eeprom["pixels"] * 2, timeout=timeout_ms)
+        self.send_cmd(0xad, 0)
+        data = self.device.read(0x82, self.pixels * 2, timeout=timeout_ms)
         if data is None:
             return
 
@@ -255,8 +273,18 @@ class Fixture(object):
         for i in range(0, len(data), 2):
             spectrum.append(data[i] | (data[i+1] << 8))
 
-        if len(spectrum) != self.eeprom["pixels"]:
+        if len(spectrum) != self.pixels:
             return
+
+        # stomp blank SiG pixels (first 3 and last)
+        for i in range(3):
+            spectrum[i] = spectrum[3]
+        spectrum[-1] = spectrum[-2]
+
+        # 2x2 binning
+        if self.args.bin2x2:
+            for i in range(self.pixels-1):
+                spectrum[i] = (spectrum[i] + spectrum[i+1]) / 2.0
 
         return spectrum
 
@@ -291,13 +319,13 @@ class Fixture(object):
         else:
             return result
 
-    def unpack(self, dev, address, data_type):
+    def unpack(self, address, data_type):
         page       = address[0]
         start_byte = address[1]
         length     = address[2]
         end_byte   = start_byte + length
 
-        buf = dev.buffers[page]
+        buf = self.buffers[page]
         if buf is None or end_byte > len(buf):
             raise("error unpacking EEPROM page %d, offset %d, len %d as %s: buf is %s (label %s)" %
                 (page, start_byte, length, data_type, buf, label))
@@ -313,5 +341,5 @@ class Fixture(object):
         return result
 
 fixture = Fixture()
-if len(fixture.devices) > 0:
+if fixture.device is not None:
     fixture.run()
