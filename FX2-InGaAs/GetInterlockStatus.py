1171db776b49ab4f8a7e99608bbcf1736c3c00b7
Mark Zieg <mark@zieg.com>
2022-03-14 17:12:55 -0400

    fix whitespace

--- FX2-InGaAs/GetInterlockStatus.py
+++ FX2-InGaAs/GetInterlockStatus.py
@@ -13,13 +13,13 @@ CAN_LASER_FIRE  = 0xef
 IS_LASER_FIRING = 0x0d
 
 def getValue(bRequest, wValue=0, wIndex=0, len_lsb=1):
-	data = dev.ctrl_transfer(DEVICE_TO_HOST, bRequest, wValue, wIndex, len_lsb, TIMEOUT_MS)
+    data = dev.ctrl_transfer(DEVICE_TO_HOST, bRequest, wValue, wIndex, len_lsb, TIMEOUT_MS)
 
     # convert response array to uint in LSB order
-	result = 0
-	for i in range(datalen):
-		result = (result << 8) | data[datalen - i - 1]
-	return result 
+    result = 0
+    for i in range(datalen):
+        result = (result << 8) | data[datalen - i - 1]
+    return result 
 
 print(f"CAN_LASER_FIRE  (0x{CAN_LASER_FIRE:02x}): {getValue(CAN_LASER_FIRE)}")
 print(f"IS_LASER_FIRING (0x{IS_LASER_FIRING:02x}): {getValue(IS_LASER_FIRING)}")
