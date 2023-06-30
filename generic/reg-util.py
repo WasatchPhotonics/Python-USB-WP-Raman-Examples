6e403ef845e224b23ef5e998f60e4a75d3ba9a14
Mark Zieg <mark@zieg.com>
2023-06-30 13:45:39 -0400

    added comboboxes, table parser

--- generic/reg-util.py
+++ generic/reg-util.py
@@ -1,191 +1,172 @@
 #!/usr/bin/python
 
+import re
 import sys
 import usb.core
 import usb.util
 import usb.control
-import argparse
 import inspect
 
 import tkinter as tk
-
-# def initUSB():
-#     devList = usb.core.find(find_all=True)
-#     for device in devList:
-#       sys.stdout.write('Decimal VendorID=' + str(device.idVendor) + ' & ProductID=' + str(device.idProduct) + '\n')
-#       sys.stdout.write('Hexadecimal VendorID=' + hex(device.idVendor) + ' & ProductID=' + hex(device.idProduct) + '\n\n')
-#       if device.idVendor == 0x24aa and device.idProduct == 0x4000:
-#         sys.stdout.write('Found Wasatch Spectrometer' + hex(device.idVendor) + ',  ' + hex(device.idProduct) + '\n')
-#         return  device
-#         break
-#     
-#     return None
-# 
-# def getFPGARevision(wdev):
-#     if(wdev != None):
-#         sys.stdout.write('Wasatch Device Captured ' + hex(wdev.idVendor) + ',  ' + hex(wdev.idProduct) + '\n')
-#         wdev.set_configuration()
-#         cfg = wdev.get_active_configuration()
-#         interface = cfg[(0,0)]
-#         rbuf = usb.util.create_buffer(7)
-#         
-#         bmReqType = usb.util.build_request_type(usb.util.CTRL_IN, usb.util.CTRL_TYPE_VENDOR,usb.util.CTRL_RECIPIENT_DEVICE)
-#         sys.stdout.write('bmReqType is ' + hex(bmReqType) + ' \n' + 'recv buf length is ' + str(len(rbuf)) + ' \n\n')
-#         wdev.ctrl_transfer(bmReqType,0x81, 0x20, 0x00, rbuf)
-#         sys.stdout.write('response from device for req = 0x81 is \n\n')
-#         print('[{}]'.format(', '.join(hex(x) for x in rbuf)))
-#         
-# def setFPGARevision(wdev):
-#         xmit_buf = usb.util.create_buffer(7)
-#         xmit_buf = [0x51, 0x72, 0x73, 0x74, 0x75, 0x77, 0x78]
-#         bmReqType = usb.util.build_request_type(usb.util.CTRL_OUT, usb.util.CTRL_TYPE_VENDOR,usb.util.CTRL_RECIPIENT_DEVICE)
-#         sys.stdout.write('XMIT bmReqType is ' + hex(bmReqType) + ' \n' + 'recv buf length is ' + str(len(rbuf)) + ' \n\n')
-#         wdev.ctrl_transfer(bmReqType,0x91, 0x00, 0, xmit_buf)
-#         
-#         bmReqType = usb.util.build_request_type(usb.util.CTRL_IN, usb.util.CTRL_TYPE_VENDOR,usb.util.CTRL_RECIPIENT_DEVICE)
-#         wdev.ctrl_transfer(bmReqType,0x81, 0x00, 0, rbuf)
-#         
-#         sys.stdout.write('response from device for req = 0x81 is \n\n')
-#         print('[{}]'.format(', '.join(hex(x) for x in rbuf)))
-#         
-# def testCtrlTransfer(wdev, reg_address):
-# 
-#     if(wdev != None):
-#         sys.stdout.write("Test " + hex(reg_address) + "\n")
-#         rbuf = usb.util.create_buffer(64)
-#         bmReqType = usb.util.build_request_type(usb.util.CTRL_IN, usb.util.CTRL_TYPE_VENDOR,usb.util.CTRL_RECIPIENT_DEVICE)
-#         wdev.ctrl_transfer(bmReqType,0x81, reg_address, 0x00, rbuf)
-#         print(bytes(rbuf).decode('utf-8'))
-#         
-# def readRegister(wdev, reg_address):
-# 
-#     if(wdev != None):
-#         sys.stdout.write("Read " + hex(reg_address) + "\n")
-#         rbuf = usb.util.create_buffer(4)
-#         bmReqType = usb.util.build_request_type(usb.util.CTRL_IN, usb.util.CTRL_TYPE_VENDOR,usb.util.CTRL_RECIPIENT_DEVICE)
-#         wdev.ctrl_transfer(bmReqType,0x81, reg_address, 0x00, rbuf)
-#         sys.stdout.write("Read Reg  @"+hex(reg_address)+" = " )
-#         print('[{}]'.format(', '.join(hex(x) for x in rbuf)))# find USB devices
-# 
-# def writeRegister(wdev, reg_address, reg_value):
-# 
-#     if(wdev != None):
-#         sys.stdout.write("Write " + hex(reg_address) + " " + hex(reg_value) + "\n")
-#         tbuf = usb.util.create_buffer(8)
-#         bmReqType = usb.util.build_request_type(usb.util.CTRL_IN, usb.util.CTRL_TYPE_VENDOR,usb.util.CTRL_RECIPIENT_DEVICE)
-#         wdev.ctrl_transfer(bmReqType,0x91, reg_address, reg_value, tbuf)
-#         sys.stdout.write("Write Reg  @"+hex(reg_address)+" = " + hex(reg_value) )
-#         print('[{}]'.format(', '.join(hex(x) for x in tbuf)))# find USB devices
-#         
-# def ReadNewArchRegisters(wdev):
-# 
-#     for reg in registerList:
-#         readRegister(wdev, reg)
-#         
-# def writeNewArchRegisters(wdev):
-#     registerList = [0x0400, 0x0005, 0x0006, 0x0007, 0x0008, 0x0009, 0x000c,
-#                     0x0010, 0x0020, 0x0024, 0x0025, 0x0026, 0x0040, 0x0041, 0x0042,
-#                     0x0044, 0x0045, 0x0046, 0x0048, 0x004c, 0x0050, 0x0054, 0x0058,
-#                     0x0059, 0x005A, 0x0080, 0x0081, 0x0084, 0x0088, 0x0089, 0x008A,
-#                     0x008c, 0x008d, 0x008e, 0x0090, 0x0091, 0x0092, 0x0094, 0x0095, 
-#                     0x00c0, 0x00c1, 0x00c2, 0x00c4, 0x00c8, 0x00c9, 0x00cc, 0x00d0,
-#                     0x00d8, 0x00dc, 0x00e0, 0x00e4, 0x00e8]
-#     
-#     values       = [0x0102, 0x0304, 0x0506, 0x0708, 0x090A, 0x0B0C, 0x0D0E,
-#                     0x0F10, 0x1112, 0x1314, 0x1516, 0x1718, 0x191A, 0x1B1C, 0x1D1E,
-#                     0x1F20, 0x2122, 0x2324, 0x2526, 0x2728, 0x292A, 0x2B2C, 0x2D2E,
-#                     0x2F30, 0x3132, 0x3334, 0x3536, 0x3738, 0x393A, 0x3B3C, 0x3D3E,
-#                     0x3F40, 0x4142, 0x4344, 0x4546, 0x4748, 0x494A, 0x4B4C, 0x4D4E,
-#                     0x4F50, 0x5152, 0x5354, 0x5556, 0x5758, 0x595A, 0x5B5C, 0x5D5E,
-#                     0x5F70, 0x7172, 0x7374, 0x7576, 0x7778]
-#                     
-#     for reg, val in zip(registerList,values) :
-#         writeRegister(wdev, reg, val)
-#         
-# def main():
-#     # loop through devices, printing vendor and product ids in decimal and hex
-#     wasatchDevice = None
-#     regaddr = None
-#     regValue = None
-#     rdwrbit = None
-#     #if(len(sys.argv) < 3):
-#     #    sys.stdout.write("15 Usage: \npython " + sys.argv[0] + " r <reg_addr>\npython " + sys.argv[0] +" w <regaddr> <regval>\n")
-#     #    return -1
-#     
-#     if(len(sys.argv) > 2):
-#         rdwrbit = sys.argv[1] # read-write bit
-#         regaddr = sys.argv[2]
-#     
-#     wasatchDevice = initUSB()
-# 
-#     match rdwrbit:
-#         case 'r':
-# 			readRegister(wasatchDevice, int(regaddr, 0))
-#         case 'w':
-#             if(len(sys.argv) < 4):
-#                 sys.stdout.write("Usage: \npython " + sys.argv[0] + " w <regaddr> <regval>\n")
-#                 return -1
-#             else:
-#                 regValue = sys.argv[3]
-#                 writeRegister(wasatchDevice, int(regaddr, 0), int(regValue,0))
-#         case 't':
-# 			testCtrlTransfer(wasatchDevice, int(regaddr, 0))               
-#         case _:
-#             ReadNewArchRegisters(wasatchDevice)
-#             writeNewArchRegisters(wasatchDevice)
-#             ReadNewArchRegisters(wasatchDevice)
-#             return -2
+from tkinter import ttk
 
 class RegisterUtil(tk.Tk):
-
-    REGISTERS = [0x0000, 0x0400, 0x0005, 0x0006, 0x0007, 0x0008, 0x0009, 0x000c,
-                 0x0010, 0x0020, 0x0024, 0x0025, 0x0026, 0x0040, 0x0041, 0x0042,
-                 0x0044, 0x0045, 0x0046, 0x0048, 0x004c, 0x0050, 0x0054, 0x0058,
-                 0x0059, 0x005A, 0x0080, 0x0081, 0x0084, 0x0088, 0x0089, 0x008A,
-                 0x008c, 0x008d, 0x008e, 0x0090, 0x0091, 0x0092, 0x0094, 0x0095, 
-                 0x00c0, 0x00c1, 0x00c2, 0x00c4, 0x00c8, 0x00c9, 0x00cc, 0x00d0,
-                 0x00d8, 0x00dc, 0x00e0, 0x00e4, 0x00e8]
 
     def __init__(self):
         super().__init__()
 
-        self.title("FPGA Register Utility")
-        
-        row = 0
+        if not self.init_usb():
+            return
 
+        self.init_table()
+        self.init_gui()
+
+    def init_usb(self):
+        self.dev = None
+        for dev in usb.core.find(find_all=True):
+            if dev.idVendor == 0x24aa and dev.idProduct == 0x4000:
+                print(f"found VID 0x{dev.idVendor:04x} PID 0x{dev.idProduct:04x}")
+                self.dev = dev
+                return True
+
+    def make_addr_combobox(self, string_var, row, col):
+        cb = ttk.Combobox(self, width=10, textvariable=string_var)
+        cb.grid(row=row, column=col)
+        cb['values'] = [self.reg[addr]["name"] for addr in self.reg.keys()]
+        cb.current()
+        return string_var
+
+    def init_gui(self):
+        self.title("FPGA Register Utility")
+
+        row = 0
         tk.Label(text="Address").grid(row=row, column=1)
         tk.Label(text="Value").grid(row=row, column=2)
 
         row += 1
-
         tk.Button(text="Write", command=self.write).grid(row=row, column=0)
-
-        self.write_addr = tk.Text(height=1, width=6)
-        self.write_addr.grid(row=row, column=1)
-
+        self.write_addr = self.make_addr_combobox(tk.StringVar(), row, 1)
         self.write_value = tk.Text(height=1, width=6)
         self.write_value.grid(row=row, column=2)
 
         row += 1
-
         tk.Button(text="Read", command=self.read).grid(row=row, column=0)
-
-        self.read_addr = tk.Text(height=1, width=6)
-        self.read_addr.grid(row=row, column=1)
-
-        self.read_values = tk.Text(height=10, width=6)
-        self.read_values.grid(row=row, column=2)
-
-        row += 1
-
-        self.log = tk.Text(height=5, width=50)
-        self.log.grid(row=row, column=0, columnspan=3)
-
-    def write(self):
-        pass
+        self.read_addr = self.make_addr_combobox(tk.StringVar(), row, 1)
+        self.read_value = tk.Text(height=1, width=6)
+        self.read_value.grid(row=row, column=2)
 
     def read(self):
-        pass
+        addr = self.read_addr.get()
+        print(f"reading addr 0x{addr:04x}")
 
-win = RegisterUtil()
-win.mainloop()
+        buf = usb.util.create_buffer(4)
+        bmReqType = usb.util.build_request_type(usb.util.CTRL_IN, usb.util.CTRL_TYPE_VENDOR, usb.util.CTRL_RECIPIENT_DEVICE)
+        self.dev.ctrl_transfer(bmReqType, 0x81, addr, 0x00, buf)
+        
+        value = 0
+        for i in len(rbuf):
+            value |= rbuf[i]
+            value <<= 8
+
+        print(f"read addr 0x{addr:04x} received 0x{value:04x}")
+
+    def write(self):
+        addr = self.write_addr.get()
+        value = int(self.get_value(self.write_addr), 16)
+        print(f"writing addr 0x{addr:04x} <- 0x{value:04x}")
+
+        buf = usb.util.create_buffer(8)
+        bmReqType = usb.util.build_request_type(usb.util.CTRL_IN, usb.util.CTRL_TYPE_VENDOR,usb.util.CTRL_RECIPIENT_DEVICE)
+        self.dev.ctrl_transfer(bmReqType, 0x91, addr, value, buf)
+
+    def init_table(self):
+        #   Name            Addr    Default Description
+        #   --------------- ------- ------- ------------------------
+        table = """
+            REG_00          0x0000  0x0210  I2C Comm defaults
+            REG_04          0x0004  0x6B0F  FPGA Revision[3] (RD Only)
+            REG_05          0x0005  0xA35C  FPGA Revision[2] (RD Only)
+            REG_06          0x0006  0x12D4  FPGA Revision[1] (RD Only)
+            REG_07          0x0007  0x0863  FPGA Revision[0] (RD Only)
+            REG_08          0x0008  0x0064  INTEGRATION_TIME[31:16]
+            REG_09          0x0009  0x0900  INTEGRATION_TIME[15:0]
+            REG_0C          0x000C  0x0C18  CONFIGURATION
+            REG_10          0x0010  0x101C  CCD_OFFSET
+            REG_14          0x0014  0x1420  CCD_GAIN
+            REG_18          0x0018  0x1824  LASER_MOD_DURATION[47:32]
+            REG_19          0x0019  0x1925  LASER_MOD_DURATION[31:16]
+            REG_1A          0x001A  0x1A26  LASER_MOD_DURATION[15:0]
+            REG_1C          0x001C  0x1C27  Spare
+            REG_1D          0x001D  0x1D38  IMX Status
+            REG_1E          0x001E  0x1E00  FPGA Status
+
+            REG_40          0x0040  0x402A  LASER_MOD_PULSE_DELAY[47:32]
+            REG_41          0x0041  0x412B  LASER_MOD_PULSE_DELAY[31:16]
+            REG_42          0x0042  0x422C  LASER_MOD_PULSE_DELAY[15:0]
+            REG_44          0x0043  0x442D  LASER_MOD_PERIOD[47:32]
+            REG_45          0x0044  0x452E  LASER_MOD_PERIOD[31:16]
+            REG_46          0x0045  0x462F  LASER_MOD_PERIOD[15:0]
+            REG_48          0x0048  0x4830  FRAME_NUMBER
+            REG_4C          0x004C  0x4C34  DATA_THRESHOLD
+            REG_50          0x0050  0x5038  STATUS
+            REG_54          0x0054  0x543C  LASER_TEMP
+            REG_58          0x0058  0x5840  LASER_MOD_PULSE_WIDTH[47:32]
+            REG_59          0x0059  0x5941  LASER_MOD_PULSE_WIDTH[31:16]
+            REG_5A          0x005A  0x5A42  LASER_MOD_PULSE_WIDTH[15:0]
+            REG_5C          0x005C  0x5C43  IMX385 Spectra
+            REG_5D          0x005D  0x5D44  IMX385 Address (Register)
+            REG_5E          0x005E  0x5E45  IMX385 Data (Register)
+
+            REG_80          0x0080  0x8046  ACT_INTEGRATION_TIME[31:16]
+            REG_81          0x0081  0x8147  ACT_INTEGRATION_TIME[15:0]
+            REG_84          0x0084  0x8448  FRAME_COUNT
+            REG_88          0x0088  0x884C  LASER_TRANS_3[47:32]
+            REG_89          0x0089  0x894D  LASER_TRANS_3[31:16]
+            REG_8A          0x008A  0x8A4E  LASER_TRANS_3[15:0]
+            REG_8C          0x008C  0x8C50  LASER_TRANS_4[47:32]
+            REG_8D          0x008D  0x8D51  LASER_TRANS_4[31:16]
+            REG_8E          0x008E  0x8E52  LASER_TRANS_4[15:0]
+            REG_90          0x0090  0x9054  LASER_TRANS_5[47:32]
+            REG_91          0x0091  0x9155  LASER_TRANS_5[31:16]
+            REG_92          0x0092  0x9256  LASER_TRANS_5[15:0]
+
+            REG_94          0x0094  0x9494  Spare
+            REG_95          0x0095  0x9595  Spare
+            REG_96          0x0092  0x9696  Spare
+            REG_97          0x0097  0x9797  Spare
+
+            REG_C0          0x00C0  0xC058  LASER_TRANS_6[47:32]
+            REG_C1          0x00C1  0xC159  LASER_TRANS_6[47:32]
+            REG_C2          0x00C2  0xC25A  LASER_TRANS_6[47:32]
+            REG_C4          0x00C4  0xC45C  LASER_SET_POINT
+            REG_C8          0x00C8  0xC860  COMPILE_OPTIONS[31:16]
+            REG_C9          0x00C9  0xC961  COMPILE_OPTIONS[15:0]
+            REG_CC          0x00CC  0xCC64  LINE_LENGTH (Last Legacy)
+            REG_D0          0x00D0  0xD068  Device ID (RD Only)
+            REG_D4          0x00D4  0xD46C  Status (RD Only)
+            REG_D8          0x00D8  0xD870  Trigger Mode
+            REG_DC          0x00DC  0xDC74  Acquisition Mode
+            REG_DD          0x00DD  0xDD78  Acquisition Delay
+            REG_E0          0x00E0  0xE07C  FPGA Configuration
+            REG_E4          0x00E4  0xE480  ADC Configuration
+            REG_E8          0x00E8  0xE881  FIFO RD Pointer
+            REG_E9          0x00E9  0xE982  FIFO WR Pointer
+        """
+        # parse the above table into a dict
+        self.reg = {}
+        for line in table.split("\n"):
+            m = re.match("^\s*(\w+)\s+0x([0-9a-f]{4})\s+0x([0-9a-f]+)\s+(.*)", line, re.IGNORECASE)
+            if m:
+                name = m.group(1)
+                addr = int(m.group(2), 16)
+                default = int(m.group(3), 16)
+                description = m.group(4)
+                self.reg[addr] = { "name": name, "address": addr, "default": default, "description": description }
+                # print(f"stored 0x{addr:04x} => {self.reg[addr]}")
+
+util = RegisterUtil()
+if not util.dev:
+    print("No ARM-based Wasatch Photonics spectrometer found")
+    sys.exit(1)
+
+util.mainloop()
