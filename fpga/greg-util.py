cf7bf87a6446ad97ab6080bc5e4d7d907634fd1e
Bryan Auz <bauz@wasatchphotonics.com>
2023-08-23 15:23:48 -0400

    updated FPGA regs, read all output formatting

--- fpga/greg-util.py
+++ fpga/greg-util.py
@@ -23,77 +23,74 @@ offline_mem = {}
 class RegisterUtil(tk.Tk):
 
     """ Copied from ENG-xxx FPGA Internal Register Bank Definitions """
+    # Copied in by BAUZ 08-23-2023
     #   Name            Addr    Default Description
     #   --------------- ------- ------- ------------------------
     REGISTERS = """
-        REG_00          0x0000  0x0210  I2C Comm defaults
-        REG_04          0x0004  0x6B0F  FPGA Revision[3] (RD Only)
-        REG_05          0x0005  0xA35C  FPGA Revision[2] (RD Only)
-        REG_06          0x0006  0x12D4  FPGA Revision[1] (RD Only)
-        REG_07          0x0007  0x0863  FPGA Revision[0] (RD Only)
-        REG_08          0x0008  0x0064  INTEGRATION_TIME[31:16]
-        REG_09          0x0009  0x0900  INTEGRATION_TIME[15:0]
-        REG_0C          0x000C  0x0C18  CONFIGURATION
-        REG_10          0x0010  0x101C  CCD_OFFSET
-        REG_14          0x0014  0x1420  CCD_GAIN
-        REG_18          0x0018  0x1824  LASER_MOD_DURATION[47:32]
-        REG_19          0x0019  0x1925  LASER_MOD_DURATION[31:16]
-        REG_1A          0x001A  0x1A26  LASER_MOD_DURATION[15:0]
-        REG_1C          0x001C  0x1C27  Spare
-        REG_1D          0x001D  0x1D38  IMX Status
-        REG_1E          0x001E  0x1E00  FPGA Status
-
-        REG_40          0x0040  0x402A  LASER_MOD_PULSE_DELAY[47:32]
-        REG_41          0x0041  0x412B  LASER_MOD_PULSE_DELAY[31:16]
-        REG_42          0x0042  0x422C  LASER_MOD_PULSE_DELAY[15:0]
-        REG_44          0x0043  0x442D  LASER_MOD_PERIOD[47:32]
-        REG_45          0x0044  0x452E  LASER_MOD_PERIOD[31:16]
-        REG_46          0x0045  0x462F  LASER_MOD_PERIOD[15:0]
-        REG_48          0x0048  0x4830  FRAME_NUMBER
-        REG_4C          0x004C  0x4C34  DATA_THRESHOLD
-        REG_50          0x0050  0x5038  STATUS
-        REG_54          0x0054  0x543C  LASER_TEMP
-        REG_58          0x0058  0x5840  LASER_MOD_PULSE_WIDTH[47:32]
-        REG_59          0x0059  0x5941  LASER_MOD_PULSE_WIDTH[31:16]
-        REG_5A          0x005A  0x5A42  LASER_MOD_PULSE_WIDTH[15:0]
-        REG_5C          0x005C  0x5C43  IMX385 Spectra
-        REG_5D          0x005D  0x5D44  IMX385 Address (Register)
-        REG_5E          0x005E  0x5E45  IMX385 Data (Register)
-
-        REG_80          0x0080  0x8046  ACT_INTEGRATION_TIME[31:16]
-        REG_81          0x0081  0x8147  ACT_INTEGRATION_TIME[15:0]
-        REG_84          0x0084  0x8448  FRAME_COUNT
-        REG_88          0x0088  0x884C  LASER_TRANS_3[47:32]
-        REG_89          0x0089  0x894D  LASER_TRANS_3[31:16]
-        REG_8A          0x008A  0x8A4E  LASER_TRANS_3[15:0]
-        REG_8C          0x008C  0x8C50  LASER_TRANS_4[47:32]
-        REG_8D          0x008D  0x8D51  LASER_TRANS_4[31:16]
-        REG_8E          0x008E  0x8E52  LASER_TRANS_4[15:0]
-        REG_90          0x0090  0x9054  LASER_TRANS_5[47:32]
-        REG_91          0x0091  0x9155  LASER_TRANS_5[31:16]
-        REG_92          0x0092  0x9256  LASER_TRANS_5[15:0]
-
-        REG_94          0x0094  0x9494  Spare
-        REG_95          0x0095  0x9595  Spare
-        REG_96          0x0092  0x9696  Spare
-        REG_97          0x0097  0x9797  Spare
-
-        REG_C0          0x00C0  0xC058  LASER_TRANS_6[47:32]
-        REG_C1          0x00C1  0xC159  LASER_TRANS_6[47:32]
-        REG_C2          0x00C2  0xC25A  LASER_TRANS_6[47:32]
-        REG_C4          0x00C4  0xC45C  LASER_SET_POINT
-        REG_C8          0x00C8  0xC860  COMPILE_OPTIONS[31:16]
-        REG_C9          0x00C9  0xC961  COMPILE_OPTIONS[15:0]
-        REG_CC          0x00CC  0xCC64  LINE_LENGTH (Last Legacy)
-        REG_D0          0x00D0  0xD068  Device ID (RD Only)
-        REG_D4          0x00D4  0xD46C  Status (RD Only)
-        REG_D8          0x00D8  0xD870  Trigger Mode
-        REG_DC          0x00DC  0xDC74  Acquisition Mode
-        REG_DD          0x00DD  0xDD78  Acquisition Delay
-        REG_E0          0x00E0  0xE07C  FPGA Configuration
-        REG_E4          0x00E4  0xE480  ADC Configuration
-        REG_E8          0x00E8  0xE881  FIFO RD Pointer
-        REG_E9          0x00E9  0xE982  FIFO WR Pointer
+        REG_00			0x0000		0x0210      I2C Comm defaults, (RD Only)
+        REG_01			0x0001		0x0005      FPGA Revision[15:0], (RD Only)
+        REG_02			0x0002		0x0000      FPGA Revision[31:16], (RD Only)
+        REG_03			0x0003      0x0000      FPGA Revision[47:32], (RD Only)
+        REG_04			0x0004		0x0000      FPGA Revision[63:48], (RD Only)
+        REG_05			0x0005		0x0064      INTEGRATION_TIME[15:0]
+        REG_06			0x0006		0x0600      INTEGRATION_TIME[31:16]
+        REG_07			0x0007		0x0707      Spare
+        REG_10			0x0010		0x1000      CCD_OFFSET
+        REG_11			0x0011		0x1100      CCD_GAIN
+        REG_12			0x0012		0x0001      COI Start  (Column of Interest)
+        REG_13			0x0013		0x01E8      COI End 
+        REG_14			0x0014		0x0009      ROI Start (Row of Interest)
+        REG_15			0x0015		0x045A      ROI End 
+        REG_16			0x0016		0x1616      Spare
+        REG_17			0x0017		0x1717      Spare
+        REG_40			0x0040		0x402A		LASER_MOD_PULSE_DELAY[15:0]
+        REG_41			0x0041		0x412B		LASER_MOD_PULSE_DELAY[31:16]
+        REG_42			0x0042   	0x422C		LASER_MOD_PULSE_DELAY[47:32]
+        REG_43			0x0043		0x432D		LASER_MOD_PERIOD[15:0]
+        REG_44			0x0044		0x442E		LASER_MOD_PERIOD[31:16]
+        REG_45			0x0045		0x452F		LASER_MOD_PERIOD[47:32]
+        REG_46			0x0046		0x4630		Laser Temp 
+        REG_47			0x0047		0x4700		Laser Control 
+        REG_50			0x0050		0x5000		LASER_SET_POINT
+        REG_51			0x0051		0x5101		LASER_MOD_PULSE_WIDTH(15 :  0)
+        REG_52			0x0052		0x5204		LASER_MOD_PULSE_WIDTH(31 :  16)
+        REG_53			0x0053		0x5308		LASER_MOD_PULSE_WIDTH(47 :  32)
+        REG_54			0x0054		0x5400		IMX_Spectra[15:0]
+        REG_55			0x0055		0x5500		x"55", IMX385_Spectra[23:16]
+        REG_56			0x0056		0x0000		IMX Address (Register)
+        REG_57			0x0057		0xFFFF		IMX Data (Register)
+        REG_80			0x0080		0x8000		DATA_THRESHOLD
+        REG_81			0x0081		0x8101		FRAME_NUMBER
+        REG_82			0x0082   	0x8201		FRAME_COUNT  
+        REG_83			0x0083		0x0000		Acquisition Mode
+        REG_84			0x0084		0x0000		Acquisition Delay
+        REG_85			0x0085		0x0001		Trigger Control[15:0]
+        REG_86			0x0086		0x8686		Spare
+        REG_87			0x0087		0x8787		Spare
+        REG_90			0x0090		0x0000		Light Control
+        REG_91			0x0091		0x0000		Single Strobe Delay
+        REG_92			0x0092		0x0000		Single Strobe Width
+        REG_93			0x0093		0x0000		Continuous Strobe Period
+        REG_94			0x0094		0x9494		Spare
+        REG_95			0x0095		0x9595		Spare 
+        REG_96			0x0096 	    0x9696		Spare
+        REG_97			0x0097		0x9797		Spare
+        REG_C0			0x00C0		0xC000		GPIO
+        REG_C1			0x00C1		0x0000		Interrupt Vector
+        REG_C2			0x00C2      0xC25A		ADC Configuration
+        REG_C3			0x00C4		0xC3C3		Spare
+        REG_C4			0x00C4		0xC4C4		Spare
+        REG_C5			0x00C5		0xC561		Read Out Time
+        REG_C6			0x00C6		0x0799		LINE_LENGTH (Last Legacy)
+        REG_C7			0x00C7		0x0101		Device ID (RD Only)
+        REG_D0			0x00D0		0x0000		FPGA Control/Config
+        REG_D1			0x00D1		0x0000		FPGA Status (RD Only)
+        REG_D2			0x00D2		0x0000		IMX Control/Status
+        REG_D3			0x00D3		0x0000		BLE Control/Status
+        REG_D4			0x00D4		0xD4D4		Spare
+        REG_D5			0x00D5		0xD4D4		Spare
+        REG_D6			0x00D6      0x0000		FIFO RD Pointer
+        REG_D7			0x00D7		0x0000		FIFO WR Pointer
     """
 
     def __init__(self):
@@ -256,15 +253,15 @@ class RegisterUtil(tk.Tk):
         print(f"read {name} 0x{addr:04x} received 0x{value:04x}")
 
     def btn_read_all_clicked(self):
-        print("Name            Addr    Value   Default Description")
-        print("--------------- ------- ------- ------- ------------------------")
+        print("Name       Addr     Value    Default   Description")
+        print("------     ------   ------   -------   ------------------------")
 
         for name in sorted(self.reg):
             addr = self.reg[name]["addr"]
             desc = self.reg[name]["desc"]
             default = self.reg[name]["default"]
             value = self.read(addr)
-            print(f"{name:16s}   0x{addr:04x}   0x{value:04x}   0x{default:04x} {desc}")
+            print(f"{name:8s}   0x{addr:04x}   0x{value:04x}   0x{default:04x}    {desc}")
 
     ############################################################################
     # methods
