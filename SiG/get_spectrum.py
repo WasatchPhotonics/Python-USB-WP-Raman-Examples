9a5662bde2a5534339fff45b8123efc056e9f3e2
Mark Zieg <mzieg@wasatchphotonics.com>
2024-03-28 18:28:47 -0400

    updates

--- SiG/get_spectrum.py
+++ SiG/get_spectrum.py
@@ -8,6 +8,7 @@ import platform
 import matplotlib.pyplot as plt
 
 from time import sleep
+from datetime import datetime
 
 VID             = 0x24aa
 PID             = 0x4000
@@ -21,11 +22,10 @@ TIMEOUT_MS      = 1000
 ################################################################################
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--integration-time-ms", type=int, default=400, help="default 400")
-parser.add_argument("--gain-db",             type=int, default=8, help="default 8")
+parser.add_argument("--integration-time-ms", type=int)
+parser.add_argument("--gain-db",             type=int)
 parser.add_argument("--delay-ms",            type=int, default=10, help="how long to delay between spectra (default 10)")
 parser.add_argument("--count",               type=int, default=1, help="how many spectra to take")
-parser.add_argument("--throwaways",          type=int, default=2, help="how many throwaways to take (default 2)")
 parser.add_argument("--pixels",              type=int, default=1952, help="default 1952")
 parser.add_argument("--plot",                action="store_true", help="display graph")
 parser.add_argument("--debug",               action="store_true", help="debug output")
@@ -50,14 +50,19 @@ if os.name == "posix":
         dev.set_configuration()
     usb.util.claim_interface(dev, 0)
 
+if args.outfile is not None:
+    with open(args.outfile, "w") as outfile:
+        outfile.write("timestamp, delay_sec, int_time_ms, gain_db, avg, spectrum")
+
 ################################################################################
 # configure
 ################################################################################
 
-if False:
+if args.integration_time_ms is not None:
     print("sending SET_INTEGRATION_TIME_MS -> %d ms" % args.integration_time_ms)
     dev.ctrl_transfer(HOST_TO_DEVICE, 0xb2, args.integration_time_ms, 0, BUF, TIMEOUT_MS)
 
+if args.gain_db is not None:
     gainDB = args.gain_db << 8 
     print("sending GAIN_DB -> 0x%04x (FunkyFloat)" % gainDB)
     dev.ctrl_transfer(HOST_TO_DEVICE, 0xb7, gainDB, 0, BUF, TIMEOUT_MS) 
@@ -66,46 +71,62 @@ if False:
 # collect
 ################################################################################
 
-spectra = []
-for i in range(args.count + args.throwaways):
-    if i != 0:
-        print(f"sleeping {args.delay_ms}ms")
+delay_sec = 0
+MAX_ERROR = 10
+last_was_success = True
+
+while True:
+    spectra = []
+    errors = 0
+    for i in range(args.count):
+        print(f"{datetime.now()} sleeping {args.delay_ms}ms...", end='')
         sleep(args.delay_ms / 1000.0)
 
-    print("sending ACQUIRE")
-    dev.ctrl_transfer(HOST_TO_DEVICE, 0xad, 0, 0, BUF, TIMEOUT_MS)
+        print("sending ACQUIRE...", end='')
+        dev.ctrl_transfer(HOST_TO_DEVICE, 0xad, 0, 0, BUF, TIMEOUT_MS)
 
-    print(f"reading {args.pixels} from bulk endpoint")
-    data = dev.read(0x82, args.pixels * 2) 
+        print(f"reading {args.pixels}px...", end='')
+        try:
+            data = dev.read(0x82, args.pixels * 2) 
+            last_was_success = True
+        except:
+            errors += 1
+            last_was_success = False
+            print(f"ERROR {errors}: waiting 1sec...")
+            sleep(1)
+            continue
 
-    print(f"read {len(data)} bytes")
-    if len(data) != args.pixels * 2:
-        print("ERROR: read %d bytes" % len(data))
-        sys.exit(1)
+        print(f"read {len(data)} bytes...", end='')
+        if len(data) != args.pixels * 2:
+            print("ERROR: read %d bytes" % len(data))
+            sys.exit(1)
 
-    if i < args.throwaways:
-        print("dumping throwaway")
-    else:
-        print("storing")
         spectrum = []
         for px in range(args.pixels):
             spectrum.append(data[px*2] | (data[px*2 + 1] << 8)) # demarshal LSB-MSB to uint16
+
+        avg = sum(spectrum)/len(spectrum)
+        print(f"avg {avg:8.2f}")
+
         spectra.append(spectrum)
+
+        result = [ datetime.now(), delay_sec, args.integration_time_ms, args.gain_db, avg ]
+        result.extend(spectrum)
+        if args.outfile:
+            with open(args.outfile, "a") as outfile:
+                outfile.write(",".join([str(value) for value in result]) + "\n")
+
+    if not last_was_success:
+        break
+
+    delay_sec += 5
+    print(f"sleeping {delay_sec}sec...")
+    sleep(delay_sec)
 
 ################################################################################
 # process 
 ################################################################################
 
-print("max = %d" % max(max(spectra)))
-print("min = %d" % min(min(spectra)))
-print("sum = %e" % sum([sum(spectrum) for spectrum in spectra]))
-
-if args.outfile:
-    print(f"writing {args.outfile}")
-    with open(args.outfile, 'w') as f:
-        for i in range(args.pixels):
-            f.write("%s\n" % ', '.join([str(a[i]) for a in spectra]))
-            
 if args.plot:
     [plt.plot(a) for a in spectra]
     plt.title(f"integration time {args.integration_time_ms}ms, gain {args.gain_db}dB, count {args.count}, throwaways {args.throwaways}, delay {args.delay_ms}ms")
---
+++ SiG/test-poll-status.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import usb.core
+import random
+from datetime import datetime
+
+from time import sleep
+
+VID             = 0x24aa
+PID             = 0x4000
+HOST_TO_DEVICE  = 0x40
+DEVICE_TO_HOST  = 0xC0
+BUF             = [0] * 8
+TIMEOUT_MS      = 1000
+
+PIXELS = 1952
+COUNT  = 20
+
+print(f"searching for spectrometer with VID 0x{VID:04x}, PID 0x{PID:04x}")
+dev = usb.core.find(idVendor=VID, idProduct=PID)
+if dev is None:
+    print("No matching spectrometer found")
+    sys.exit(1)
+
+def get_poll_status():
+    result = dev.ctrl_transfer(DEVICE_TO_HOST, 0xd4, 0, 0, BUF, TIMEOUT_MS)
+    if result is not None:
+        return int(result[0])
+
+if os.path.exists("test.csv"):
+    os.remove("test.csv")
+
+int_time_ms = 400
+gain_db = 8
+
+# dev.ctrl_transfer(HOST_TO_DEVICE, 0xb2, int_time_ms, 0, BUF, TIMEOUT_MS)
+# dev.ctrl_transfer(HOST_TO_DEVICE, 0xb7, gain_db << 8, 0, BUF, TIMEOUT_MS)
+
+for iteration in range(COUNT):
+    print(f"\n=========== Iteration {iteration+1} of {COUNT} ============\n")
+    if False:
+        pass
+    elif random.random() < 0.33:
+        int_time_ms = random.randrange(100, 1000)
+        print(f"setting integration time to {int_time_ms}ms")
+        dev.ctrl_transfer(HOST_TO_DEVICE, 0xb2, int_time_ms, 0, BUF, TIMEOUT_MS)
+    elif random.random() < 0.66:
+        gain_db = random.randrange(0, 24)
+        print(f"setting gain to {gain_db}db")
+        dev.ctrl_transfer(HOST_TO_DEVICE, 0xb7, gain_db << 8, 0, BUF, TIMEOUT_MS)
+
+    stable = True
+    while not stable:
+        print("waiting for stabilization...", end='')
+        status = get_poll_status()
+        print(f"status 0x{status:02x}")
+        stable = 0 == status
+        sleep(1)
+    
+    print("reading spectra")
+    for i in range(COUNT):
+        print(f"sending ACQUIRE {i+1:2d}/{COUNT}...", end='')
+        dev.ctrl_transfer(HOST_TO_DEVICE, 0xad, 0, 0, BUF, TIMEOUT_MS)
+
+        print(f"reading {PIXELS}px...", end='')
+        data = dev.read(0x82, PIXELS * 2) 
+
+        if len(data) != PIXELS * 2:
+            print(f"ERROR: read {len(data)} bytes")
+            sys.exit(1)
+        print(f"read {len(data)} bytes...", end='')
+
+        spectrum = []
+        for j in range(PIXELS):
+            spectrum.append(data[j] | (data[j+1] << 8))
+        lo = min(spectrum)
+        hi = max(spectrum)
+        avg = sum(spectrum)/PIXELS
+        print(f"min {lo:5d}, max {hi: 5d}, avg {avg:8.2f}")
+
+        # save data to outfile
+        result = [ datetime.now(), iteration, int_time_ms, gain_db, lo, hi, avg ]
+        result.extend(spectrum)
+        with open("test.csv", "a") as outfile:
+            outfile.write(",".join([str(value) for value in result]) + "\n")
+
+        sleep(0.2)
