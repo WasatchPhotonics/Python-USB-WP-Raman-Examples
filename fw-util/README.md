b2ada28015c46efcdc98274d0c54432a9a114b4c
Justin Klein <jklein@wasatchphotonics.com>
2024-05-15 16:46:38 -0400

    initial working gui

---
+++ fw-util/README.md
@@ -0,0 +1,20 @@
+
+# JLink FW Flashing Tool
+Provides a minimal GUI for flashing STM and BLE chipsets.
+
+# How to install and use
+1. Clone project locally: `git clone https://github.com/WasatchPhotonics/Python-USB-WP-Raman-Examples.git`
+2. Create and activate virtual environment under the `fw-util/` directory
+   1.  `cd fw-util`
+   2.  `python -m venv venv`
+   3. `. venv/bin/activate`
+3. Install dependencies
+   1.   `pip install .`
+4. Launch GUI
+   1.   `python fw_util.py`
+5. Connect Segger JLink to appropriate pins
+6. Select whether to flash `BLE652` or `STM32`. For `BLE652` there is an optional erase checkbox that will erase it first.
+7. Click the `Flash` button and watch the magic happen.
+
+# Requirements
+- JLink `https://www.segger.com/downloads/jlink/` (tested with V7.6G)
---
+++ fw-util/fw_util.py
@@ -0,0 +1,99 @@
+"""
+fw_util - Minimal GUI for flashing STM and BLE
+"""
+
+import pexpect
+import tkinter as tk
+import time
+
+# Harcoded for now.  Maybe these should move into a config file
+JLINK_EXE="/Applications/SEGGER/JLink/JLinkExe"
+ERASE_HEX="s132_nrf52_7.0.1_softdevice.hex"
+BLE_HEX="170086_sig_ble_nrf_v4.4.0.hex"
+STM_HEX="170112_v01_0_17_1_170113_v01_4_15.hex"
+BLE_PN="NRF52832_XXAA"
+STM_PN="STM32H753VI"
+
+def button_flash_clicked(selection, erase):
+    """Send appropriate commands to the JLinkEXE"""
+
+    # Build list of commands based on input
+    if selection == 'BL652':
+
+        if erase:
+            erase_cmd_rsp = {"erase":".*Erasing done.*",
+                             "h":".*FPSCR.*",
+                             f"loadfile {ERASE_HEX}":".*O.K..*",
+                             "r":".*Reset device.",
+                             "g":".* is active.*",}
+        else:
+            erase_cmd_rsp = {}
+
+        cmd_rsp = { "connect" : ".*Type.*",
+                    f"{BLE_PN}": ".*cJTAG.*",
+                    "S" : ".*Default.*",
+                    "4000 kHz":"Cortex-M4",
+                    **erase_cmd_rsp,
+                    "h": ".*FPSCR.*",
+                    f"loadfile {BLE_HEX}" :".*O.K.*",
+                    "r": ".*Reset device.*",
+                    "g": ".*is active.*",
+                    "exit": None}
+
+    if selection == 'STM32':
+        # Dictionary of commands and expected responses to check for
+        # If None, then ignored
+        cmd_rsp = { "connect" : ".*Type.*",
+                    f"{STM_PN}" : ".*cJTAG.*",
+                    "S" : ".*Default.*",
+                    "4000 kHz" : ".*4000 kHz.*",
+                    "h" : ".*J-Link>.*",
+                    f"loadfile {STM_HEX}" : f".*O.K..*",
+                    "r" : ".*Reset device.*",
+                    "g" : ".*is active.*",
+                    "exit" : ".OnDisconnectTarget.*"}
+
+    # Write to temp file and call .exe
+    ps = pexpect.spawn(f"{JLINK_EXE}", encoding='utf-8')
+
+    for cmd, rsp in cmd_rsp.items():
+        print(f"Sending:\n {cmd}, expecting: {rsp}")
+        ps.sendline(cmd)
+        if rsp is not None:
+            ps.expect(rsp)
+
+    time.sleep(1)
+
+    ps.close()
+
+    print("done")
+
+
+def main():
+    # Create main window
+    root = tk.Tk()
+    root.title("Firmware flash tool")
+
+    # Variables to store checkbox states
+    erase = tk.BooleanVar()
+    sel = tk.StringVar()
+
+    # Create buttons
+    ble_rbutton = tk.Radiobutton(root, text="BL652", variable=sel, value='BL652')
+    stm_rbutton = tk.Radiobutton(root, text="STM32", variable=sel, value='STM32')
+    sel.set('BL652')
+
+    chkbox_erase = tk.Checkbutton(root, text="Erase", variable=erase)
+    button_flash = tk.Button(root, text="Flash", command= lambda: button_flash_clicked(sel.get(), erase.get()))
+
+    #Place
+    ble_rbutton.pack()
+    chkbox_erase.pack()
+    stm_rbutton.pack()
+    button_flash.pack()
+
+    # Start the Tkinter event loop
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
