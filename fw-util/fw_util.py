e6ce99a17072022dfb540a4d444fc18a14935160
Justin Klein <jklein@wasatchphotonics.com>
2024-05-15 17:10:31 -0400

    cleanup comments

--- fw-util/fw_util.py
+++ fw-util/fw_util.py
@@ -1,5 +1,5 @@
 """
-fw_util - Minimal GUI for flashing STM and BLE
+fw_util.py - Minimal GUI for flashing STM and BLE
 """
 
 import pexpect
@@ -14,10 +14,10 @@ STM_HEX="170112_v01_0_17_1_170113_v01_4_15.hex"
 BLE_PN="NRF52832_XXAA"
 STM_PN="STM32H753VI"
 
-def button_flash_clicked(selection, erase):
+def do_flash(selection, erase):
     """Send appropriate commands to the JLinkEXE"""
 
-    # Build list of commands based on input
+    # Build dictionary of commands / expected responses based on GUI settings
     if selection == 'BL652':
 
         if erase:
@@ -41,8 +41,6 @@ def button_flash_clicked(selection, erase):
                     "exit": None}
 
     if selection == 'STM32':
-        # Dictionary of commands and expected responses to check for
-        # If None, then ignored
         cmd_rsp = { "connect" : ".*Type.*",
                     f"{STM_PN}" : ".*cJTAG.*",
                     "S" : ".*Default.*",
@@ -53,9 +51,10 @@ def button_flash_clicked(selection, erase):
                     "g" : ".*is active.*",
                     "exit" : ".OnDisconnectTarget.*"}
 
-    # Write to temp file and call .exe
+    # Spawn a child application
     ps = pexpect.spawn(f"{JLINK_EXE}", encoding='utf-8')
 
+    # Loop through each command / response
     for cmd, rsp in cmd_rsp.items():
         print(f"Sending:\n {cmd}, expecting: {rsp}")
         ps.sendline(cmd)
@@ -66,7 +65,7 @@ def button_flash_clicked(selection, erase):
 
     ps.close()
 
-    print("done")
+    print("Done.")
 
 
 def main():
@@ -74,7 +73,7 @@ def main():
     root = tk.Tk()
     root.title("Firmware flash tool")
 
-    # Variables to store checkbox states
+    # Variables to store checkbox/radiobutton states
     erase = tk.BooleanVar()
     sel = tk.StringVar()
 
@@ -84,7 +83,7 @@ def main():
     sel.set('BL652')
 
     chkbox_erase = tk.Checkbutton(root, text="Erase", variable=erase)
-    button_flash = tk.Button(root, text="Flash", command= lambda: button_flash_clicked(sel.get(), erase.get()))
+    button_flash = tk.Button(root, text="Flash", command= lambda: do_flash(sel.get(), erase.get()))
 
     #Place
     ble_rbutton.pack()
