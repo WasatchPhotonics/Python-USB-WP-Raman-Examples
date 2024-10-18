6f7a5b9c5726ab99a4986c9daa14f758bd33c0bf
Mark Zieg <mzieg@wasatchphotonics.com>
2024-10-18 16:32:17 -0400

    restructured

--- generic/ble-util.py
+++ generic/ble-util.py
@@ -7,6 +7,10 @@ class Fixture:
     WASATCH_SERVICE   = "D1A7FF00-AF78-4449-A34F-4DA1AFAF51BC"
     DISCOVERY_SERVICE = "0000ff00-0000-1000-8000-00805f9b34fb"
 
+    ############################################################################
+    # Lifecycle
+    ############################################################################
+
     def __init__(self):
         self.stop_event = asyncio.Event()
         self.client = None
@@ -32,48 +36,40 @@ class Fixture:
         parser.add_argument("--debug",               action="store_true", help="debug output")
         parser.add_argument("--timeout-sec",         type=int,            help="how long to search for spectrometers", default=30)
         parser.add_argument("--serial-number",       type=str,            help="delay n ms between spectra")
+
+        # not yet implemented
         parser.add_argument("--laser-enable",        action="store_true", help="fire the laser")
-        parser.add_argument("--max-sec",             type=int,            help="how long to perform the test", default=5)
+        parser.add_argument("--read-eeprom",         action="store_true", help="load and parse the EEPROM")
+        parser.add_argument("--scan-averaging",      type=int,            help="set scans to average", default=1)
+        parser.add_argument("--integration-time-ms", type=int,            help="set integration time", default=400)
+        parser.add_argument("--gain-db",             type=float,          help="set gain (dB)", default=8)
+        parser.add_argument("--spectra",             type=int,            help="spectra to acquire", default=5)
+        parser.add_argument("--outfile",             type=str,            help="save spectra to CSV file")
+        parser.add_argument("--monitor",             action="store_true", help="monitor battery, laser state etc")
         self.args = parser.parse_args()
 
-    def wrap_uuid(self, code):
-        return f"d1a7{code:04x}-af78-4449-a34f-4da1afaf51bc".lower()
+    async def main(self):
+        self.start_time = datetime.now()
+        print(f"{datetime.now()} rssi local_name")
+        async with BleakScanner(detection_callback=self.detection_callback, service_uuids=[self.WASATCH_SERVICE]) as scanner:
+            await self.stop_event.wait()
 
-    def get_char_name_by_uuid(self, uuid):
-        return self.char_name_by_uuid.get(uuid.lower(), None)
-        
-    def get_char_uuid(self, name):
-        name = name.upper()
-        if name not in self.char_code_by_name:
+        # scanner stops when block exits
+        self.debug("scanner stopped")
+
+        if self.client is None:
+            if self.args.serial_number:
+                print(f"{self.args.serial_number} not found")
             return
-        return self.wrap_uuid(self.characteristics[name])
 
-    def stop_scanning(self):
-        self.stop_event.set()
+        # connect 
+        await self.client.connect()
 
-    def dump(self, device, advertisement_data):
-        self.debug("==> Device:")
-        for attr in ['name', 'address', 'details']:
-            if hasattr(device, attr):
-                value = getattr(device, attr)
-                self.debug(f"  {attr} = {value}")
-        self.debug("==> Advertising Data:")
-        for attr in ['local_name', 'manufacturer_data', 'platform_data', 'rssi', 'service_data', 'service_uuids', 'tx_power']:
-            if hasattr(advertisement_data, attr):
-                value = getattr(advertisement_data, attr)
-                self.debug(f"  {attr} = {value}")
+        # grab device information
+        await self.get_device_information()
 
-    def debug(self, msg):
-        if self.args.debug:
-            print(f"{datetime.now()} DEBUG: {msg}")
-
-    def is_xs(self, device, advertisement_data=None):
-        if device is None:
-            return
-        elif advertisement_data is not None:
-            return self.WASATCH_SERVICE.lower() in advertisement_data.service_uuids
-        else:
-            return "wp-" in device.name.lower()
+        # get Characteristic information
+        await self.get_characteristics()
 
     def detection_callback(self, device, advertisement_data):
         """
@@ -114,31 +110,16 @@ class Fixture:
         self.stop_event.set()
         self.debug(f"BleakClient instantiated: {self.client}")
 
+    def stop_scanning(self):
+        self.stop_event.set()
+
     def disconnected_callback(self):
         print("\ndisconnected")
 
-    async def main(self):
-
-        self.start_time = datetime.now()
-        print(f"{datetime.now()} rssi local_name")
-        async with BleakScanner(detection_callback=self.detection_callback, service_uuids=[self.WASATCH_SERVICE]) as scanner:
-            await self.stop_event.wait()
-
-        # scanner stops when block exits
-        self.debug("scanner stopped")
-
-        if self.client:
-            await self.test_client()
-
-    def decode(self, data):
-        try:
-            if isinstance(data, bytearray):
-                return data.decode('utf-8')
-        except:
-            pass
-            return data
-
     async def get_device_information(self):
+        print(f"address = {self.client.address}")
+        print(f"mtu_size = {self.client.mtu_size} bytes")
+
         self.device_info = {}
         for service in self.client.services:
             if "Device Information" in str(service):
@@ -147,23 +128,11 @@ class Fixture:
                     value = self.decode(await self.client.read_gatt_char(char.uuid))
                     self.device_info[name] = value
 
-    async def test_client(self):
-        print(f"address = {self.client.address}")
-        print(f"mtu_size = {self.client.mtu_size} bytes")
-
-        # 0x2a26 Firmware Version   STM32 Version (e.g. "1.0.43.2")
-        # 0x2a27 Hardware Revision  FPGA Version (e.g. "01.4.28")
-        # 0x2a28 Software Version   BL652 Version (e.g. "4.8.5")
-        # 0x2a29 Manufacturer Name  "Wasatch Photonics"
-
-        await self.client.connect()
-
-        # grab device information
-        await self.get_device_information()
         print("Device Information:")
         for k, v in self.device_info.items():
             print(f"  {k:24s} = {v}")
 
+    async def get_characteristics(self):
         # find the primary service
         self.primary_service = None
         for service in self.client.services:
@@ -197,41 +166,77 @@ class Fixture:
         # async BleakClient.read_gatt_char (char_specifier: Union[BleakGATTCharacteristic, int, str, UUID], **kwargs)→ bytearray
         # async BleakClient.write_gatt_char(char_specifier: Union[BleakGATTCharacteristic, int, str, UUID], data: Buffer, response: bool = None)→ None
 
+    ############################################################################
+    # Utility
+    ############################################################################
+
+    def debug(self, msg):
+        if self.args.debug:
+            print(f"{datetime.now()} DEBUG: {msg}")
+
+    def wrap_uuid(self, code):
+        return f"d1a7{code:04x}-af78-4449-a34f-4da1afaf51bc".lower()
+
+    def get_char_name_by_uuid(self, uuid):
+        return self.char_name_by_uuid.get(uuid.lower(), None)
+        
+    def get_char_uuid(self, name):
+        name = name.upper()
+        if name not in self.char_code_by_name:
+            return
+        return self.wrap_uuid(self.characteristics[name])
+
+    def dump(self, device, advertisement_data):
+        self.debug("==> Device:")
+        for attr in ['name', 'address', 'details']:
+            if hasattr(device, attr):
+                value = getattr(device, attr)
+                self.debug(f"  {attr} = {value}")
+        self.debug("==> Advertising Data:")
+        for attr in ['local_name', 'manufacturer_data', 'platform_data', 'rssi', 'service_data', 'service_uuids', 'tx_power']:
+            if hasattr(advertisement_data, attr):
+                value = getattr(advertisement_data, attr)
+                self.debug(f"  {attr} = {value}")
+
+    def is_xs(self, device, advertisement_data=None):
+        if device is None:
+            return
+        elif advertisement_data is not None:
+            return self.WASATCH_SERVICE.lower() in advertisement_data.service_uuids
+        else:
+            return "wp-" in device.name.lower()
+
+    def decode(self, data):
+        try:
+            if isinstance(data, bytearray):
+                return data.decode('utf-8')
+        except:
+            pass
+        return data
+
 fixture = Fixture()
 asyncio.run(fixture.main())
 
 """
-Service d1a7ff00-af78-4449-a34f-4da1afaf51bc (Handle: 11): Unknown
-  Characteristic d1a7ff01-af78-4449-a34f-4da1afaf51bc (Handle: 12): Unknown (read,write) , Value:
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 14): Characteristic User Description, Value: Integration Time
-  Characteristic d1a7ff02-af78-4449-a34f-4da1afaf51bc (Handle: 15): Unknown (read,write) , Value: bytearray(b'\xaf')
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 17): Characteristic User Description, Value: Gain
-  Characteristic d1a7ff03-af78-4449-a34f-4da1afaf51bc (Handle: 18): Unknown (read,write,notify) , Value:
-
-    Descriptor 00002902-0000-1000-8000-00805f9b34fb (Handle: 20): Client Characteristic Configuration, Value:
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 21): Characteristic User Description, Value: Laser Enable
-  Characteristic d1a7ff04-af78-4449-a34f-4da1afaf51bc (Handle: 22): Unknown (write)
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 24): Characteristic User Description, Value: Acquire Spectrum
-  Characteristic d1a7ff05-af78-4449-a34f-4da1afaf51bc (Handle: 25): Unknown (read,write) , Value:
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 27): Characteristic User Description, Value: Spectrum Command
-  Characteristic d1a7ff06-af78-4449-a34f-4da1afaf51bc (Handle: 28): Unknown (read,write,indicate) , Value: bytearray(b'\xff\xff')
-    Descriptor 00002902-0000-1000-8000-00805f9b34fb (Handle: 30): Client Characteristic Configuration, Value:
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 31): Characteristic User Description, Value: Read Spectrum
-  Characteristic d1a7ff07-af78-4449-a34f-4da1afaf51bc (Handle: 32): Unknown (read,write) , Value: bytearray(b'\xad')
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 34): Characteristic User Description, Value: EEPROM Command
-  Characteristic d1a7ff08-af78-4449-a34f-4da1afaf51bc (Handle: 35): Unknown (read,indicate) , Value: bytearray(b'\xe1')
-    Descriptor 00002902-0000-1000-8000-00805f9b34fb (Handle: 37): Client Characteristic Configuration, Value:
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 38): Characteristic User Description, Value: EEPROM Data
-  Characteristic d1a7ff09-af78-4449-a34f-4da1afaf51bc (Handle: 39): Unknown (read,notify) , Value:
-
-    Descriptor 00002902-0000-1000-8000-00805f9b34fb (Handle: 41): Client Characteristic Configuration, Value:
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 42): Characteristic User Description, Value: Battery Status
-  Characteristic d1a7ff0a-af78-4449-a34f-4da1afaf51bc (Handle: 43): Unknown (read,write,indicate) , Value:
-    Descriptor 00002902-0000-1000-8000-00805f9b34fb (Handle: 45): Client Characteristic Configuration, Value:
-    Descriptor 00002901-0000-1000-8000-00805f9b34fb (Handle: 46): Characteristic User Description, Value: Generic
-Service 0000180a-0000-1000-8000-00805f9b34fb (Handle: 47): Device Information
-  Characteristic 00002a29-0000-1000-8000-00805f9b34fb (Handle: 48): Manufacturer Name String (read) , Value: Wasatch Photonics
-  Characteristic 00002a27-0000-1000-8000-00805f9b34fb (Handle: 50): Hardware Revision String (read) , Value: 4
-  Characteristic 00002a26-0000-1000-8000-00805f9b34fb (Handle: 52): Firmware Revision String (read) , Value: 01.4.28
-  Characteristic 00002a28-0000-1000-8000-00805f9b34fb (Handle: 54): Software Revision String (read) , Value: 4.7.3
+MacBook-Pro.lan [~/work/code/Python-USB-WP-Raman-Examples/generic] mzieg  4:06PM $ python -u ble-util.py --serial-number WP-01791
+2024-10-18 16:06:49.828516 rssi local_name
+2024-10-18 16:06:51.426465  -71 WP-SiG:WP-01791
+address = 13874014-5EDA-5E6B-220E-605D00FE86DF
+mtu_size = 515 bytes
+Device Information:
+  Manufacturer Name String = Wasatch Photonics
+  Hardware Revision String = 4
+  Firmware Revision String = 01.4.28
+  Software Revision String = 4.7.3
+Characteristics:
+  Characteristic INTEGRATION_TIME d1a7ff01-af78-4449-a34f-4da1afaf51bc (read,write), Value:
+  Characteristic GAIN_DB          d1a7ff02-af78-4449-a34f-4da1afaf51bc (read,write), Value: bytearray(b'\xaf')
+  Characteristic LASER_STATE      d1a7ff03-af78-4449-a34f-4da1afaf51bc (read,write,notify), Value:
+  Characteristic ACQUIRE_SPECTRUM d1a7ff04-af78-4449-a34f-4da1afaf51bc (write)
+  Characteristic SPECTRUM_COMMAND d1a7ff05-af78-4449-a34f-4da1afaf51bc (read,write), Value:
+  Characteristic READ_SPECTRUM    d1a7ff06-af78-4449-a34f-4da1afaf51bc (read,write,indicate), Value: bytearray(b'\xff\xff')
+  Characteristic EEPROM_COMMAND   d1a7ff07-af78-4449-a34f-4da1afaf51bc (read,write), Value: bytearray(b'\xad')
+  Characteristic EEPROM_DATA      d1a7ff08-af78-4449-a34f-4da1afaf51bc (read,indicate), Value: bytearray(b'\xe1')
+  Characteristic BATTERY_STATUS   d1a7ff09-af78-4449-a34f-4da1afaf51bc (read,notify), Value:
+  Characteristic GENERIC_MESSAGE  d1a7ff0a-af78-4449-a34f-4da1afaf51bc (read,write,indicate), Value:
 """
