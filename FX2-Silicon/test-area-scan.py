a0e4fc89a26aeff0149c1e0d4a2e89f30d9c19d2
Mark Zieg <mzieg@wasatchphotonics.com>
2025-04-10 09:58:24 -0400

    added cmd-line args

--- FX2-Silicon/test-area-scan.py
+++ FX2-Silicon/test-area-scan.py
@@ -4,7 +4,11 @@ import sys
 import usb.core
 import time
 import numpy as np
+import argparse
 import matplotlib.pyplot as plt
+import matplotlib.image
+
+from datetime import datetime
 
 H2D = 0x40
 D2H = 0xC0
@@ -34,14 +38,29 @@ class Fixture:
         self.frame_count = 0
         self.total_line_count = 0
 
+        self.args = self.parse_args()
+
         self.frame = [ [0] * PIXELS for line in range(LINES) ] # 70 lines, each of 1024 pixels
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.add_argument("--debug",               action="store_true", help="debug output")
+        parser.add_argument("--integration-time-ms", type=int,            help="set integration time", default=100)
+        parser.add_argument("--frames",              type=int,            help="how many frames to collect (0=infinite)", default=5)
+        parser.add_argument("--plot",                action="store_true", help="graph frames")
+        parser.add_argument("--save",                action="store_true", help="save frames as PNG")
+        return parser.parse_args()
 
     def connect(self):
         self.dev = usb.core.find(idVendor=0x24aa, idProduct=0x1000)
         return self.dev is not None
 
+    def debug(self, msg):
+        if self.args.debug:
+            print(f"{datetime.now()} DEBUG: {msg}")
+
     def send_trigger(self):
-        print("sending ACQUIRE software trigger...")
+        self.debug("sending ACQUIRE software trigger...")
         self.dev.ctrl_transfer(H2D, 0xad, 0, 0, Z, TIMEOUT) 
 
     def get_spectrum(self):
@@ -51,7 +70,7 @@ class Fixture:
         while len(data) < LINE_LEN:
             bytes_remaining = LINE_LEN - len(data)
             latest_data = self.dev.read(0x82, bytes_remaining, timeout=TIMEOUT)
-            # print(f"read {len(latest_data)} bytes ({bytes_remaining} requested)")
+            self.debug(f"read {len(latest_data)} bytes ({bytes_remaining} requested): {latest_data[:10]}")
             data.extend(latest_data) 
 
         extra_len = len(data) - LINE_LEN
@@ -71,11 +90,11 @@ class Fixture:
         return spectrum
     
     def set_integration_time_ms(self, ms):
-        print(f"setting integration time to {ms}ms")
+        self.debug(f"setting integration time to {ms}ms")
         self.dev.ctrl_transfer(H2D, 0xb2, ms, 0, Z, TIMEOUT) 
 
     def set_area_scan_enable(self, flag):
-        print(f"area scan enable = {flag}")
+        self.debug(f"area scan enable = {flag}")
         self.dev.ctrl_transfer(H2D, 0xeb, 1 if flag else 0, 0, Z, TIMEOUT) 
 
     def eat_throwaways(self):
@@ -83,45 +102,52 @@ class Fixture:
         try:
             while True:
                 throwaway = self.get_spectrum()
-                print(f"consumed throwaway {count}: {throwaway[:5]}")
+                self.debug(f"consumed throwaway {count}: {throwaway[:5]}")
                 count += 1
         except usb.core.USBTimeoutError:
             pass
 
         if count == 0:
-            print("no throwaways to consume")
+            self.debug("no throwaways to consume")
         else:
-            print(f"consumed {count} throwaways")
+            print(f"WARNING: consumed {count} throwaways")
 
-    def display_frame(self):
+    def process_frame(self):
+        if not (self.args.plot or self.args.save):
+            return
+
         image = np.array(self.frame)
-
         sums = image.sum(axis=1)
         normalized = image / sums[:, np.newaxis]
+        scaled = normalized * 65535
 
-        plt.imshow(normalized, cmap='gray') 
-        plt.title(f"Frame {self.frame_count}")
-        plt.show(block=False)
-        plt.pause(3)
-        time.sleep(1)
+        if self.args.save:
+            filename = f"areascan-{self.frame_count:03d}.png"
+            matplotlib.image.imsave(filename, scaled)
+            self.debug("saved {filename}")
+
+        if self.args.plot:
+            plt.imshow(scaled, cmap='gray') 
+            plt.title(f"Frame {self.frame_count}")
+            plt.show(block=False)
+            plt.pause(1)
+            # time.sleep(1)
 
     def run(self):
-        self.set_integration_time_ms(100)
+        self.set_integration_time_ms(self.args.integration_time_ms)
 
-        print("eating initial throwawys (no ACQUIRE)")
+        self.debug("eating initial throwawys (no ACQUIRE)")
         self.eat_throwaways()
 
-        print("sending one ACQUIRE, then eating throwaways")
+        self.debug("sending one ACQUIRE, then eating throwaways")
         self.send_trigger()
         self.eat_throwaways()
 
-        # enable area scan
-        print("enabling area scan, then eating throwaways")
+        self.debug("enabling area scan, then eating throwaways")
         self.set_area_scan_enable(True)
         self.eat_throwaways()
 
-        # ready to start reading frames of area scan data
-        while True:
+        while self.frame_count < self.args.frames or 0 == self.args.frames:
             # request a frame
             self.send_trigger()
 
@@ -132,6 +158,7 @@ class Fixture:
                 # first pixel is start-of-line marker
                 if spectrum[0] != MARKER:
                     print(f"{prefix}: WARNING: first pixel expected 0x{MARKER:04x}, found 0x{spectrum[0]:04x}")
+                spectrum[0] = spectrum[2] # stomp
 
                 # verify rest of line is clamped to 0xfffe
                 for i, intensity in enumerate(spectrum):
@@ -142,25 +169,26 @@ class Fixture:
                 line_index = spectrum[1]
                 if line_index >= LINES:
                     print(f"{prefix}: WARNING: line_index {line_index} exceeds frame limit")
-                else:
-                    # store line in image
-                    self.frame[line_index] = spectrum
-                    print(f"{prefix}: stored line {line_index}: {spectrum[:5]}")
+                spectrum[1] = spectrum[2] # stomp
+
+                # store line in image
+                self.frame[line_index] = spectrum
+                self.debug(f"{prefix}: stored line {line_index}: {spectrum[:5]}")
                 
-                # periodically display image
+                # process completed frame
                 self.line_count += 1
                 if line_index + 1 == LINES:
-                    print(f"{prefix}: displaying frame {self.frame_count}")
-
-                    self.display_frame()
+                    self.debug(f"{prefix}: displaying frame {self.frame_count}")
+                    self.process_frame()
 
                     # initialize for next frame
                     self.line_count = 0
                     self.frame_count += 1
-                    print(f"\nreading frame {self.frame_count}")
+                    self.debug(f"\nreading frame {self.frame_count}")
 
 if __name__ == "__main__":
     fixture = Fixture()
     if not fixture.connect():
+        print("No spectrometer found.")
         sys.exit(-1)
     fixture.run()
