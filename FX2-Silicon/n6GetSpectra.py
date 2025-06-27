2b1c2fa29689be794c009febd8d15bf0526275bd
ram krishnan <ram.krishnan@wasatchphotonics.com>
2025-06-27 17:10:18 -0400

    changes

--- FX2-Silicon/n6GetSpectra.py
+++ FX2-Silicon/n6GetSpectra.py
@@ -19,7 +19,7 @@ H2D=0x40
 D2H=0xC0
 BUFFER_SIZE=1
 Z=[0] * BUFFER_SIZE
-TIMEOUT=15000
+TIMEOUT=3000
 MAX_SEC = 1000
 INTEG_MS = 235
 
@@ -31,11 +31,32 @@ PixelCount=2592*1944
 
 start_time = datetime.datetime.now()
 
+
+def img_print_row(row_nr):
+    print("row ", row_nr, ": ", 
+          hex(spectrum[(2592*row_nr)]), 
+          ", ",
+          hex(spectrum[(2592*row_nr) + 1]),
+          ", ",
+          hex(spectrum[(2592*row_nr) + 2]),
+          ", ",
+          hex(spectrum[(2592*row_nr) + 3]),
+          ", ",
+          hex(spectrum[(2592*row_nr) + 4]),
+          ", ",
+          hex(spectrum[(2592*row_nr) + 5]),
+          ", ",
+          hex(spectrum[(2592*row_nr) + 6]),
+          ", ",
+          hex(spectrum[(2592*row_nr) + 7]))
+
 firstSegRcvd = 0
 count = 0
-while (datetime.datetime.now() - start_time).total_seconds() < MAX_SEC:
+while (1):  # datetime.datetime.now() - start_time).total_seconds() < MAX_SEC:
+    firstSegRcvd = 0
     print("sending acquire")
     # dev.ctrl_transfer(H2D, 0xad, 0, 0, Z, TIMEOUT)   # trigger an acquisition
+
     data = dev.ctrl_transfer(D2H, 0xb4, 0, 0, 1, TIMEOUT)
     print("reading data from EP 0x82 .....\n")
 
@@ -43,16 +64,24 @@ while (datetime.datetime.now() - start_time).total_seconds() < MAX_SEC:
     print("TS0:", ts0)
     cumulative_data = []
     total_bytes_needed = PixelCount*2 
+    tot_read = 0
     while len(cumulative_data) < total_bytes_needed:
         bytes_remaining = total_bytes_needed - len(cumulative_data)
-        #print(f"requesting {bytes_remaining} bytes with timeout {TIMEOUT}ms")
-        latest_data = dev.read(0x82, bytes_remaining, timeout=TIMEOUT)
+        read_cnt = bytes_remaining
+        if bytes_remaining > 512:
+           read_cnt = 512
+
+        # print(f"requesting {read_cnt} bytes with timeout {TIMEOUT}ms")
+        latest_data = []
+        latest_data = dev.read(0x82, read_cnt, timeout=TIMEOUT)
         if firstSegRcvd == 0:
             print("first seg sz rcvd", len(latest_data))
             ts1 = time.time()
             print("TS1:", ts1)
             firstSegRcvd = 1
         # print("read %d bytes (%d requested)" % (len(latest_data), bytes_remaining))
+        tot_read += len(latest_data)
+        # print("tot rd", tot_read)
         # print(bytes_remaining)
         cumulative_data.extend(latest_data)
 
@@ -80,26 +109,17 @@ while (datetime.datetime.now() - start_time).total_seconds() < MAX_SEC:
     #   x_axis.append(i)
 
     print("Rcvd spectrum with pixel cnt", len(spectrum))
-    print(hex(spectrum[0]), 
-          hex(spectrum[1]),
-          hex(spectrum[2]),
-          hex(spectrum[3]),
-          hex(spectrum[4]),
-          hex(spectrum[5]),
-          hex(spectrum[6]),
-          hex(spectrum[7]))
+    img_print_row(0)
+    img_print_row(1)
+    img_print_row(2)
+    img_print_row(1942)
+    img_print_row(1943)
+   
+
     pixCnt = len(spectrum)
-    print(hex(spectrum[pixCnt-8]), 
-          hex(spectrum[pixCnt-7]),
-          hex(spectrum[pixCnt-6]),
-          hex(spectrum[pixCnt-5]),
-          hex(spectrum[pixCnt-4]),
-          hex(spectrum[pixCnt-3]),
-          hex(spectrum[pixCnt-2]),
-          hex(spectrum[pixCnt-1]))
     packed_bytes = b"".join([struct.pack('B', byte_val) for byte_val in cumulative_data])
 
-    file_path = "raw10.bin"
+    file_path = "raw10_11.raw"
     with open(file_path, "wb") as f:
       f.write(packed_bytes)
       print(f"Binary array saved to {file_path}")
